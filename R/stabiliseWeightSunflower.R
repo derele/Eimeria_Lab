b25820fa9aca90db1172aaa5cce21a4f89585021
Alice Balard <alice.balard@fu-berlin.de>
2018-05-11 17:14:37 +0200

    Update husbandry analysis

--- R/stabiliseWeightSunflower.R
+++ R/stabiliseWeightSunflower.R
@@ -1,5 +1,8 @@
-sunFlo <- read.csv("../data/3_recordingTables/Preliminary_April2018_wildmice_Eferrisi_second_RECORDweight .csv")
+source("dataPreparation.R")
+batch1 <- ExpeDFMay2018batch1
+batch2 <- read.csv("../data/3_recordingTables/Preliminary_April2018_wildmice_Eferrisi_second_RECORDweight .csv")
 
+#############
 library(ggplot2)
 mytheme <- theme_bw()+
   theme(plot.title = element_text(size=22), plot.subtitle = element_text(size = 20),
@@ -9,24 +12,14 @@ mytheme <- theme_bw()+
         legend.key = element_rect(size = 5),
         legend.key.size = unit(1.5, 'lines'),
         strip.text = element_text(size=25))
+#############
 
-sunFlo$prelim_labels <- as.character(sunFlo$prelim_labels)
 
-ggplot(sunFlo) +
-  geom_point(aes(x = date, y = weight)) +
-  geom_line(aes(x = date, y = weight, group = prelim_labels)) +
-  mytheme
+## Earliest weight we had
+originalWeight <- batch1[batch1$dpi == -7, ]
 
-begin <- sunFlo[sunFlo$date == "07/05/18", c("prelim_labels", "weight")]
-plus3Days <- sunFlo[sunFlo$date == "10/05/18", c("prelim_labels", "weight")]
+table(originalWeight$weight, originalWeight$Mouse_strain)
 
-names(begin)[2] <- "weight_begin"      
-names(plus3Days)[2] <- "weight_plus3Days"      
-
-tot <- merge(begin, plus3Days, all = T)
-
-tot$weightDiff <- tot$weight_plus3Days - tot$weight_begin
-
-ggplot(tot) +
-  geom_violin(aes(x = "Difference of weight", y = weightDiff))+
-  mytheme
+by(data = originalWeight, 
+   INDICES = list(originalWeight$sex, originalWeight$Mouse_strain), 
+   FUN = function(x) mean(x$weight))
