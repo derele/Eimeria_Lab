3fb3257b57a6eb46058c1f2185800fa36500e989
Alice Balard <alice.balard@fu-berlin.de>
2018-05-15 12:37:29 +0200

    Clean up eimeria lab

--- R/analysis.R
+++ R/analysis.R
@@ -1,288 +1,158 @@
-library(ggplot2)
-library(gridExtra)
-library(reshape2)
-library(scales)
-
-mytheme <- theme_bw()+
-  theme(plot.title = element_text(size=22), plot.subtitle = element_text(size = 20),
-        legend.text = element_text(size = 20),
-        axis.text=element_text(size=20),
-        axis.title=element_text(size=20,face="bold"),
-        legend.key = element_rect(size = 5),
-        legend.key.size = unit(1.5, 'lines'),
-        strip.text = element_text(size=25))
-
-########################### Data loading #######################################
-# ExpeDF need at least the following: 
-# "OPG", "EH_ID", "dpi", "weight", "EH_id", "infection_isolate"                   
-
 source("dataPreparation.R")
+source("summarySE.R")
 
-ExpeDF <- ExpeDFMay2018batch1
+###########################################
+# Choose the experiment:
 
-## Weight evolution
-plainWeight <- ggplot(ExpeDF, 
-                      aes(x = dpi, y = weight))+
-  geom_line(col = "black", aes(group = EH_ID)) +
-  geom_point(size=3, pch = 21, color = "black", aes(fill = infection_isolate), alpha = 0.78) +
-  mytheme +
-  facet_wrap(~Mouse_strain, scales = "free_y")+
-  scale_x_continuous(breaks = -7:11, name = "Day post infection (dpi)" )
-plot(plainWeight)
+##### Expe_001
+# March 2017, Francisca's experiment. infection with E64 and Eflab
+ExpeDF <- ExpeDF_001
 
-# compared to dpi 0
-plotWeight <- ggplot(ExpeDF, 
-                     aes(x = dpi, y = weightRelativeToInfection))+
+##### Expe_002
+
+##### Expe_003
+# April-May 2018, first batch. Parental strains (F0) BUSNA, STRA, SCHUNT, PWD
+# Infection with Eferrisi (E64 and E139)
+ExpeDF <- ExpeDF_003
+# remove stabilisation period
+ExpeDF <- ExpeDF[ExpeDF$dpi %in% 0:11,]
+
+###########################################
+## Weight evolution compared to dpi 0
+ggplot(ExpeDF, aes(x = dpi, y = weightRelativeToInfection))+
   geom_line(col = "black", aes(group = EH_ID), alpha = 0.5) +
-  geom_point(size=3, pch = 21, color = "black", aes(fill = infection_isolate), alpha = 0.78) +
+  geom_point(aes(fill = infection_isolate), 
+              size=3, pch = 21, color = "black") +
   mytheme +
-  facet_wrap(~Mouse_strain, scales = "free_y")+
-  scale_x_continuous(breaks = -7:11, name = "Day post infection (dpi)" )+
-  geom_smooth(aes(col = infection_isolate)) +
-  annotate("rect", xmin=-Inf, xmax=0, ymin=-Inf, ymax=Inf, alpha=0.6, fill="grey") 
-plot(plotWeight)
-
-
-## PLOT mice strains:
-# PlotOoFollow <- ggplot(ExpeDF, aes(x=dpi, y=OPG, group = Mouse_strain, col = Mouse_strain))+
-#   geom_smooth(aes(fill = Mouse_strain), alpha = 0.2)+
-#   ggtitle("Oocyst count at different days post infection (dpi)", 
-#           subtitle = "Loess smoothing + 95% CI")+
-#   scale_x_continuous(breaks = 0:11) +
-#   facet_wrap(~infection_isolate,  scales="free_y") +
-#   geom_jitter(width=0.1, size=5, pch = 21, color = "black", aes(fill = Mouse_strain), alpha = 0.78) +
-#   scale_y_continuous(labels = scientific) +
-#   mytheme 
-# plot(PlotOoFollow)
-# 
-# PlotOoTotFollow <- ggplot(ExpeDF, aes(x=as.factor(dpi), y=oocystsTotal, group = Mouse_strain, col = Mouse_strain))+
-#   geom_smooth(aes(fill = Mouse_strain), alpha = 0.2)+
-#   ggtitle("Oocyst count at different days post infection (dpi)", 
-#           subtitle = "Loess smoothing + 95% CI")+
-#   # scale_x_continuous(breaks = 0:11) +
-#   facet_wrap(~infection_isolate,  scales="free_y") +
-#   geom_jitter(width=0.1, size=5, pch = 21, color = "black", aes(fill = Mouse_strain), alpha = 0.78) +
-#   scale_y_continuous(labels = scientific) +
-#   mytheme 
-# plot(PlotOoTotFollow)
-
-
-
-library(reshape2)
-
-df <- na.omit(melt(ExpeDF, id.vars = c("EH_ID", "dpi"), measure.vars = c("weight")))
-
-df <- df[df$variable == "weight",c("EH_ID", "dpi", "value")]
-
-df1 <- df[df$dpi =="-7", ]
-df2 <- df[df$dpi =="-1", ]
-df <- merge(df1, df2, by = "EH_ID", all = T)
-
-df$diff <- (df$value.x - df$value.y) / df$value.x * 100
-mean(df$diff)
-
+  facet_wrap(~Mouse_strain)+
+  scale_x_continuous(breaks = 0:11, name = "Day post infection (dpi)" )+
+  geom_smooth(aes(col = infection_isolate)) 
 
 # Mean + 95%CI
-source("summarySE.R")
-summaryOocysts <-summarySE(ExpeDF, measurevar="oocystsTotal", 
-                           groupvars=c("Mouse_strain","infection_isolate", "dpi"))
+summaryWeight <-summarySE(ExpeDF, measurevar = "weightRelativeToInfection", 
+                           groupvars=c("Mouse_strain", "infection_isolate", "dpi"))
 
-ggplot(summaryOocysts, aes(x=as.factor(dpi), y=oocystsTotal, group = Mouse_strain, col = Mouse_strain))+
-  geom_errorbar(aes(ymin=oocystsTotal-ci, ymax=oocystsTotal+ci), colour="black", width=.1, 
-                position=position_dodge(0.1)) +
-  geom_line(position=position_dodge(0.1)) +
-  geom_point(position=position_dodge(0.1), size=3) +
-  facet_wrap(~infection_isolate, scales="free_y") +
-  scale_x_continuous(breaks = 0:11) +
-  mytheme 
+ggplot(summaryWeight, aes(x = dpi, y = weightRelativeToInfection))+
+  geom_errorbar(aes(ymin = weightRelativeToInfection - ci,
+                    ymax = weightRelativeToInfection + ci,
+                    col = infection_isolate)) +
+  geom_line(aes(group = infection_isolate)) +
+  geom_point(aes(fill = infection_isolate), 
+             size=3, pch = 21, color = "black") +
+  mytheme +
+  facet_wrap(~Mouse_strain)+
+  scale_x_continuous(breaks = 0:11, name = "Day post infection (dpi)") +
+  scale_y_continuous(name = "Weight relative to infection (%)" )
 
-# Weight
+# maximum weight lost along experiment
+all.max.loss <- do.call("rbind", by(ExpeDF,
+                                ExpeDF$EH_ID, function (x){
+  m.loss <- which(x$weightRelativeToInfection == min(x$weightRelativeToInfection,
+                                                     na.rm=TRUE))
+  x[m.loss,]
+}))
 
-PlotWeight <- ggplot(ExpeDF, aes(x=dpi, y=weightRelativeToInfection))+
-  geom_smooth(aes(fill = Mouse_strain, col = infection_isolate), alpha = 0.2) +
-  #scale_x_continuous(breaks = 0:11) +
-  facet_wrap(~infection_isolate) +
-  geom_point(pch = 21, color = "black", size = 3) +
-  mytheme 
-plot(PlotWeight)
-# 
-# ###############################
-# # Input data recent:
-# #ExpeDF <- read.csv("../data_clean/May2017_crossing_infection.csv")
-# #ExpeDF <- read.csv(file = "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data_clean/May2017_crossing_infection.csv")
-# ###############################
-# ## Part 1: West should always be left 
-# # ExpeDF$strain <- factor(ExpeDF$strain, levels = c("WSB", "WP", "PWD"))
-# 
-# # Plot to follow the weight : if < 80% weight, mouse has to be sacrificed
-# PlotWeightFollow <- ggplot(data=ExpeDF[ExpeDF$dpi > 0,], # from day 1
-#                            aes(x=dpi, y=weightRelativeToInfection)) +
-#   geom_line(aes(group = EH_ID, color = infection_isolate))+
-#   geom_point(aes(color = infection_isolate), size = 3, alpha = 0.5)+
-#   theme_bw()+
-#   theme(legend.position=c(.15, .1), legend.title = element_text(size=20, face="bold"),
-#         legend.text = element_text(size = 20),
-#         axis.text=element_text(size=20),
-#         axis.title=element_text(size=20,face="bold")) +
-#   scale_x_continuous(breaks = 1:11, name = "Day post infection (dpi)" )
-# 
-# plot(PlotWeightFollow)
-# 
-# ## PLOT mice strains:
-# PlotOoFollow <- ggplot(ExpeDF, aes(x=dpi, y=log10(OPG +1), group = Mouse_strain, col = Mouse_strain))+
-#   geom_smooth(aes(fill = Mouse_strain), alpha = 0.2)+
-#   ggtitle("Oocyst count at different days post infection (dpi)", 
-#           subtitle = "Loess smoothing + 95% CI")+
-#   scale_x_continuous(breaks = 0:11) +
-#   facet_wrap(~infection_isolate) +
-#   geom_jitter(width=0.1, size=5, pch = 21, color = "black", aes(fill = Mouse_strain), alpha = 0.78) +
-#   # scale_color_manual(values=c("blue", "purple", "red"),
-#   #                    name="Mice genotypes",
-#   #                    breaks=c("WSB", "WP", "PWD"),
-#   #                    labels=c("M.m.domesticus (West)", "Hybrid", "M.m.musculus (East)"))+
-#   # scale_fill_manual(values=c("blue", "purple", "red"),
-#   #                   name="Mice genotypes",
-#   #                   breaks=c("WSB", "WP", "PWD"),
-#   #                   labels=c("M.m.domesticus (West)", "Hybrid", "M.m.musculus (East)"))+
-#   # labs(y = "Oocyst per gram", x = "Day post infection (dpi)") +
-#   scale_y_continuous(labels = scientific) +
-#   mytheme 
-# plot(PlotOoFollow)
-# 
-# PlotOoTotFollow <- ggplot(ExpeDF, aes(x=dpi, y=log10(oocystsTotal +1), group = Mouse_strain, col = Mouse_strain))+
-#   geom_smooth(aes(fill = Mouse_strain), alpha = 0.2)+
-#   ggtitle("Oocyst count at different days post infection (dpi)", 
-#           subtitle = "Loess smoothing + 95% CI")+
-#   scale_x_continuous(breaks = 0:11) +
-#   facet_wrap(~infection_isolate) +
-#   geom_jitter(width=0.1, size=5, pch = 21, color = "black", aes(fill = Mouse_strain), alpha = 0.78) +
-#   # scale_color_manual(values=c("blue", "purple", "red"),
-#   #                    name="Mice genotypes",
-#   #                    breaks=c("WSB", "WP", "PWD"),
-#   #                    labels=c("M.m.domesticus (West)", "Hybrid", "M.m.musculus (East)"))+
-#   # scale_fill_manual(values=c("blue", "purple", "red"),
-#   #                   name="Mice genotypes",
-#   #                   breaks=c("WSB", "WP", "PWD"),
-#   #                   labels=c("M.m.domesticus (West)", "Hybrid", "M.m.musculus (East)"))+
-#   # labs(y = "Oocyst per gram", x = "Day post infection (dpi)") +
-#   scale_y_continuous(labels = scientific) +
-#   mytheme 
-# plot(PlotOoTotFollow)
-# 
-# 
-# 
-# ### To clean
-# # Violin plots of the total sum of oocysts collected during 11 days: 
-# sum.oocysts <- do.call("rbind", by(ExpeDF, ExpeDF$EH_ID, function (x){
-#   x$sum.oo <- sum(x$OPG, na.rm=TRUE)
-#   x
-# }))
-# 
-# sum.oocysts <- sum.oocysts[!duplicated(sum.oocysts$EH_ID),]
-# # NB: some mice died before!!
-# 
-# # sum.oocysts$strain <- factor(sum.oocysts$strain,
-# #                              levels = c("WSB", "WP", "PWD"))
-# # 
-# # my_title <- expression(paste("Sum of oocysts shed during experimental ", 
-# #                              italic("E. ferrisi"), " infection"))
-# 
-# PlotOoSum <- ggplot(sum.oocysts, #
-#                     aes(Mouse_strain, sum.oo)) +
-#   ggtitle(my_title) + 
-#   geom_violin(color = "black")+
-#   geom_jitter(width=0.1, size=7, alpha = 0.8,
-#               pch = 21, aes(fill = Mouse_strain)) +
-#   labs(y = "Oocyst count", x = "Mouse strain") +
-#   facet_grid(.~infection_isolate) +
-#   # scale_color_manual(values=c("blue", "purple", "red"))+
-#   # scale_fill_manual(values=c("blue", "purple", "red"))+
-#   theme_bw()
-#   # theme(plot.title = element_text(size=22), plot.subtitle = element_text(size = 20),
-#   #       axis.text=element_text(size=20),
-#   #       axis.title=element_text(size=20,face="bold"),
-#   #       strip.text = element_text(size=25),
-#   #       legend.position="none") +
-#   # scale_x_discrete(labels=c("WSB" = expression(italic("M.m.domesticus")),
-#   #                           "WP" = "hybrids",
-#   #                           "PWD" = expression(italic("M.m.musculus"))))
-# 
-# #pdf(file="../figures/May2017_oocyst_sum.pdf", width=12, height=8)
-# plot(PlotOoSum)
-# #dev.off()
-# 
-# # mean and 95%CI
-# library(Rmisc)
-# aggregate(sum.oocysts$sum.oo, 
-#           list(sum.oocysts$infection_isolate, sum.oocysts$Mouse_strain), 
-#           CI, ci=0.95)
-# 
-# ## Some stats on the oocysts :
-# sum.oocysts$sum.oo <- round(sum.oocysts$sum.oo, 0)
-# levels(sum.oocysts$strain) <- c(0, 0.5, 1)
-# sum.oocysts$strain
-# sum.oocysts$strain <- as.numeric(as.character(sum.oocysts$strain))
-# 
-# kruskal.test(sum.oo ~ strain, data = sum.oocysts[sum.oocysts$Inf_strain == "EI64",])
-# 
-# # glm.hybrid::glm.hybrid(formula = sum.oo ~ strain, data = sum.oocysts, alpha.along = "strain")
-# 
-# # maximum weight lost before death
-# max.loss <- do.call("rbind", by(ExpeDF, ExpeDF$EH_id, function (x){
-#   m.loss <- which(x$rel.weight==min(x$rel.weight, na.rm=TRUE))
-#   x[m.loss,]
-# }))
-# 
-# max.loss$strain <- factor(max.loss$strain,
-#                           levels = c("WSB", "WP", "PWD"))
-# 
-# max.loss$max_loss <- 100 - max.loss$rel.weight
-# 
-# table(max.loss$dpi, max.loss$strain)
-# 
-# table(max.loss$dpi, max.loss$Inf_strain)
-# 
-# table(max.loss$dpi, max.loss$Inf_strain, max.loss$strain)
-# 
-# tapply(max.loss$rel.weight, max.loss$Inf_strain:max.loss$strain, mean)
-# 
-# my_title <- expression(paste("Maximum weight loss during experimental ", 
-#                              italic("E. ferrisi"), " infection"))
-# 
-# PlotWeightMax <- ggplot(max.loss[max.loss$Inf_strain == "EI64",],
-#                         aes(strain, max_loss, color=Inf_strain)) +
-#   ggtitle(my_title) +
-#   geom_violin(color = "black")+
-#   geom_jitter(width=0.1, size=7, pch = 21,
-#               color = "black", aes(fill = strain), alpha = 0.8) +
-#   labs(y= "Weight loss (%)", x= "Mouse strain")+
-#   scale_color_manual(values=c("blue", "purple", "red"))+
-#   scale_fill_manual(values=c("blue", "purple", "red"))+
-#   theme_bw()+
-#   theme(plot.title = element_text(size=22), plot.subtitle = element_text(size = 20),
-#         axis.text=element_text(size=20),
-#         axis.title=element_text(size=20,face="bold"),
-#         strip.text = element_text(size=25),
-#         legend.position="none") +
-#   scale_x_discrete(labels=c("WSB" = expression(italic("M.m.domesticus")),
-#                             "WP" = "hybrids",
-#                             "PWD" = expression(italic("M.m.musculus"))))
-# 
-# #pdf(file="../figures/May2017_weight_max.pdf", width=12, height=8)
-# plot(PlotWeightMax)
-# #dev.off()
-# 
-# summary(glm(rel.weight~strain + Inf_strain, data=max.loss))
-# 
-# summary(glm(rel.weight~strain, data = max.loss[max.loss$Inf_strain == "EI64",]))
-# 
-# kruskal.test(rel.weight ~ strain, data = max.loss[max.loss$Inf_strain == "EI64",])
-# summary(aov(rel.weight ~ strain, data = max.loss[max.loss$Inf_strain == "EI64",]))
-# 
-# summary(max.loss$rel.weight[max.loss$Inf_strain == "EI64" & 
-#                               max.loss$strain == "WSB"])
-# 
-# summary(max.loss$rel.weight[max.loss$Inf_strain == "EI64" & 
-#                               max.loss$strain == "PWD"])
-# 
-# ## using offsets and real weight instead of relative weight for modeling
+all.max.loss$weightLossRelativeToInfection <- 100 - all.max.loss$weightRelativeToInfection
+
+# What is the mean of weight loss?
+max.loss <- all.max.loss[!duplicated(all.max.loss$EH_ID),]
+
+tapply(max.loss$weightRelativeToInfection,
+       max.loss$infection_isolate:max.loss$Mouse_strain, 
+       mean)
+
+ggplot(max.loss,
+       aes(Mouse_strain, weightLossRelativeToInfection)) +
+  geom_violin(color = "black")+
+  geom_jitter(width=0.1, size=7, alpha = 0.8,
+              pch = 21, aes(fill = Mouse_strain)) +
+  labs(y = "Maximum weight loss along infection", x = "Mouse strain") +
+  facet_grid(.~infection_isolate) +
+  mytheme +
+  theme(legend.position = "none")
+
+if (length(levels(ExpeDF$infection_isolate)) <2){
+  summary(glm(data = max.loss, weightLossRelativeToInfection ~ Mouse_strain))
+} else {
+  summary(glm(data = max.loss, weightLossRelativeToInfection ~ Mouse_strain*infection_isolate))
+}
+## using offsets and real weight instead of relative weight for modeling
+
+###########################################
+# oocyst shedding evolution
+ggplot(ExpeDF, aes(x = dpi, y = OPG))+
+  geom_line(col = "black", aes(group = EH_ID), alpha = 0.5) +
+  geom_point(aes(fill = infection_isolate), 
+             size=3, pch = 21, color = "black", alpha = 0.78) +
+  mytheme +
+  facet_wrap(~Mouse_strain)+
+  scale_x_continuous(breaks = 0:11, name = "Day post infection (dpi)" )+
+  geom_smooth(aes(col = infection_isolate)) +
+  scale_y_continuous(labels = scientific)
+  
+# Mean + 95%CI
+summaryOocysts <-summarySE(ExpeDF, measurevar="OPG", 
+                           groupvars=c("Mouse_strain", "infection_isolate", "dpi"))
+
+ggplot(summaryOocysts, aes(x = dpi, y = OPG))+
+  geom_errorbar(aes(ymin = OPG - ci,
+                    ymax = OPG + ci,
+                    col = infection_isolate)) +
+  geom_line(aes(group = infection_isolate)) +
+  geom_point(aes(fill = infection_isolate), 
+             size=3, pch = 21, color = "black") +
+  mytheme +
+  facet_wrap(~Mouse_strain)+
+  scale_x_continuous(breaks = 0:11, name = "Day post infection (dpi)") +
+  annotate("rect", xmin=-Inf, xmax=0, ymin=-Inf, ymax=Inf, alpha=0.6, fill="grey")
+
+# maximum OPG within expe
+all.max.shed <- do.call("rbind", 
+                        by(ExpeDF,
+                           ExpeDF$EH_ID, function (x){
+                             m.loss <- which(x$OPG == max(x$OPG, na.rm=TRUE))
+                             x[m.loss,]}))
+
+# Violin plots of the total OPG along 11 days: 
+all.sum.oocysts <- do.call("rbind", by(ExpeDF, ExpeDF$EH_ID, function (x){
+  x$sum.oo <- sum(x$OPG, na.rm = TRUE)
+  x
+}))
+sum.oocysts <- all.sum.oocysts[!duplicated(all.sum.oocysts$EH_ID),]
+
+ggplot(sum.oocysts,
+       aes(Mouse_strain, sum.oo/1000000)) +
+  geom_violin(color = "black")+
+  geom_jitter(width=0.1, size=7, alpha = 0.8,
+              pch = 21, aes(fill = Mouse_strain)) +
+  labs(y = "Sum OPG along infection (millions)", x = "Mouse strain") +
+  facet_grid(.~infection_isolate) +
+  mytheme +
+  theme(legend.position = "none")
+
+summary(glm(data = sum.oocysts, sum.oo ~ Mouse_strain))
+
+############################
+# highest day of shedding vs highest weight loss?
+
+shedVsLossdpi <- merge(data.frame(EH_ID = all.max.loss$EH_ID, 
+                                  dpi_maxLoss = all.max.loss$dpi),
+                       data.frame(EH_ID = all.max.shed$EH_ID, 
+                                  dpi_maxShed = all.max.shed$dpi), all = T)
+
+shedVsLossdpi <- merge(shedVsLossdpi, 
+                       unique(ExpeDF[c("EH_ID", "infection_isolate", "Mouse_strain")]), 
+                       all = T)
+shedVsLossdpi$diffMaxLossMaxShed <- shedVsLossdpi$dpi_maxLoss - shedVsLossdpi$dpi_maxShed
+
+table(shedVsLossdpi$diffMaxLossMaxShed, shedVsLossdpi$infection_isolate, shedVsLossdpi$Mouse_strain)
+
+ggplot(shedVsLossdpi, aes(x = diffMaxLossMaxShed,
+                          y = infection_isolate)) +
+  geom_jitter(aes(fill = Mouse_strain), 
+              size=3, pch = 21, color = "black",
+              position = position_jitter(.1, .1)) +
+  mytheme +
+  theme(legend.position = "none") +
+  annotate
+  
--- R/dataPreparation.R
+++ R/dataPreparation.R
@@ -1,38 +1,80 @@
 ## Data preparation
+library(ggplot2)
+library(gridExtra)
+library(reshape2)
+library(scales)
 
+mytheme <- theme_bw()+
+  theme(plot.title = element_text(size=22), plot.subtitle = element_text(size = 20),
+        legend.text = element_text(size = 20),
+        axis.text=element_text(size=20),
+        axis.title=element_text(size=20,face="bold"),
+        legend.key = element_rect(size = 5),
+        legend.key.size = unit(1.5, 'lines'),
+        strip.text = element_text(size=25))
+
+# functions used
+calculateWeightLoss <- function(ExpeDF){
+  A = ExpeDF[ExpeDF$dpi == 0, c("weight", "EH_ID")]
+  names(A)[1] = "weightAtInfection"
+  ExpeDF <- merge(ExpeDF, A)
+  rm(A)
+  ExpeDF$weightloss = ExpeDF$weightAtInfection - ExpeDF$weight
+  ExpeDF$weightRelativeToInfection <- ExpeDF$weight /
+    ExpeDF$weightAtInfection * 100
+  return(ExpeDF)
+}
+
+
+# ExpeDF need at least the following: 
+# "OPG", "EH_ID", "dpi", "weight", "EH_id", "infection_isolate", "Mouse_strain"  
+
+########################### Exp001 : April 2017 PWD, WSD and hybrids infection 
+# by E.falciformis (Eflab) and E.ferrisi (E64)
+ExpeDF_001 <- read.csv("../data/3_recordingTables/Exp001_May2017_crossing_infection.csv")
+# Remove Eflab, mice suffered too much and had to be sacrificed earlier
+ExpeDF_001 <- ExpeDF_001[ExpeDF_001$infection_isolate %in% "EI64",]
+ExpeDF_001$infection_isolate <- droplevels(ExpeDF_001$infection_isolate)
+
+# Mouse_strain: West should always be left 
+ExpeDF_001$Mouse_strain <- factor(ExpeDF_001$Mouse_strain, 
+                                  levels = c("WSB", "WP", "PWD"),
+                                  labels = c("M.m.domesticus \n(WSB)", 
+                                             "F1 hybrids \n(WP)", 
+                                             "M.m.musculus \n(PWD)"))
+# Keep only mice that survived 11 days
+survivors <- names(table(ExpeDF_001$EH_ID))[table(ExpeDF_001$EH_ID) %in% 12]
+ExpeDF_001 <- ExpeDF_001[ExpeDF_001$EH_ID %in% survivors,]
+
+########################### Exp002: March 2018 NMRI infected with
 # ExpeDF <- read.csv("../data/3_recordingTables/March2018_NMRI_4strains_RECORDweightAndOocysts.csv")
 # # general
 # ExpeDF$Mouse_strain <- "NMRI"
 
-########################### May 2018 batch 1
-oo <- read.csv("../data/3_recordingTables/April2018_wildmice_Eferrisi_Firstbatch_RECORDoocysts.csv")
-we <- read.csv("../data/3_recordingTables/April2018_wildmice_Eferrisi_Firstbatch_RECORDweight.csv")
-design <- read.csv("../data/2_designTables/April2018_wildmice_Eferrisi_Firstbatch_DESIGN.csv")
+########################### Exp003 : May 2018 batch 1
+oo <- read.csv("../data/3_recordingTables/Exp003_April2018_wildmice_Eferrisi_Firstbatch_RECORDoocysts.csv")
+we <- read.csv("../data/3_recordingTables/Exp003_April2018_wildmice_Eferrisi_Firstbatch_RECORDweight.csv")
+design <- read.csv("../data/2_designTables/Exp003_April2018_wildmice_Eferrisi_Firstbatch_DESIGN.csv")
 
-ExpeDFMay2018batch1 <- merge(oo, we, all = T)
-ExpeDFMay2018batch1 <- merge(ExpeDFMay2018batch1, design, by = "EH_ID", all = T)
+ExpeDF_003 <- merge(oo, we, all = T)
+ExpeDF_003 <- merge(ExpeDF_003, design, by = "EH_ID", all = T)
+rm(design, oo, we)
+
+# correct abherante value
+ExpeDF_003[ExpeDF_003$EH_ID %in% "LM0137" & ExpeDF_003$weight %in% 17.6, "weight"] <- NA
+
+# Mouse_strain: West should always be left 
+ExpeDF_003$Mouse_strain <- factor(ExpeDF_003$Mouse_strain,
+                                  levels = c("STRA", "BUSNA", "SCHUNT", "PWD"),
+                                  labels = c("M.m.domesticus \n(STRA)", 
+                                             "M.m.musculus \n(BUSNA)", 
+                                             "M.m.domesticus \n(SCHUNT)",
+                                             "M.m.musculus \n(PWD)"))
 
 # Calculate weight loss
-A <- ExpeDFMay2018batch1[ExpeDFMay2018batch1$dpi == 0, c("weight", "EH_ID")]
-names(A)[1] <- "weightAtInfection"
-ExpeDFMay2018batch1 <- merge(ExpeDFMay2018batch1, A)
-
-A <- ExpeDFMay2018batch1[ExpeDFMay2018batch1$dpi == -7, c("weight", "EH_ID")]
-names(A)[1] <- "weightAtAnthelminthicTrt"
-ExpeDFMay2018batch1 <- merge(ExpeDFMay2018batch1, A)
-
-ExpeDFMay2018batch1$weightloss <- ExpeDFMay2018batch1$weightAtInfection - ExpeDFMay2018batch1$weight
-
-ExpeDFMay2018batch1$weightRelativeToInfection <- ExpeDFMay2018batch1$weight /
-  ExpeDFMay2018batch1$weightAtInfection * 100
-
-ExpeDFMay2018batch1$weightRelativeToAnthelmTrtDay <- ExpeDFMay2018batch1$weight /
-  ExpeDFMay2018batch1$weightAtAnthelminthicTrt * 100
+ExpeDF_003 <- calculateWeightLoss(ExpeDF = ExpeDF_003)
 
 # Calculate OPG
 # ExpeDFMay2018batch1$mean_Neubauer <- (ExpeDFMay2018batch1$Neubauer1 + ExpeDFMay2018batch1$Neubauer2 + ExpeDFMay2018batch1$Neubauer3 + ExpeDFMay2018batch1$Neubauer4) / 4
 # ExpeDFMay2018batch1$OPG <- ExpeDFMay2018batch1$mean_Neubauer * 10000 / ExpeDFMay2018batch1$dilution_ml / ExpeDFMay2018batch1$fecweight
 # ExpeDFMay2018batch1$oocystsTotal <- ExpeDFMay2018batch1$mean_Neubauer * 10000 / ExpeDFMay2018batch1$dilution_ml
-
-# remove junk
-rm(A, design, oo, we)
