05aa397c8d29395aa42820d775ebdfd2103a5987
Alice Balard <alice.balard@fu-berlin.de>
2018-07-02 14:51:14 +0200

    begin to prepare design

--- R/makeDesignTable.R
+++ R/makeDesignTable.R
@@ -1,24 +1,64 @@
-makeDesignTable <- function(myseed, pathToInfoTable, firstEH_Id, Inf_strains_list){
-  # Load information table
-  infoTable = read.csv(pathToInfoTable)
-  
-  Nmice = nrow(infoTable)
+# makeDesignTable <- function(myseed, pathToInfoTable, firstEH_Id, Inf_strains_list){
+# Load information table
+infoTable = read.csv(pathToInfoTable)
 
-  # Give EH_ids
-  num = as.numeric(sub("LM", "", firstEH_Id))
-  num = num + (1:(Nmice))
-  EH_id = paste0("LM", sprintf("%04d", num))
+Nmice = nrow(infoTable)
 
-  # Spread names randomly among mice
-  set.seed(myseed)
-  infoTable$EH_id <- sample(EH_id)
+# Give EH_ids
+num = as.numeric(sub("LM", "", firstEH_Id))
+num = num + (1:(Nmice))
+EH_id = paste0("LM", sprintf("%04d", num))
 
-  # Spread infection equally among sex and Mouse_strain
-  table(infoTable$Sex, infoTable$Strain)
-  return(infoTable)
-}
+# Spread names randomly among mice
+set.seed(myseed)
+infoTable$EH_id <- sample(EH_id)
 
-myDesignTable2 <- makeDesignTable(myseed = 1234 ,
+# Spread infection equally among sex and Mouse_strain
+table(infoTable$Sex, infoTable$Strain)
+return(infoTable)
+# }
+
+# raw <- read.csv("../data/1_informationTables/rawData/Exp005_WDS_21.6.18.csv")
+# 
+# raw$Parent1 = raw$Strain
+# raw$Parent2 = raw$Sir
+# raw$Strain = paste0(raw$Parent1, "_", raw$Parent2)
+# 
+# raw$HybridStatus[
+#   raw$Strain %in% c("BUSNA_BUSNA","SCHUNT_SCHUNT", "STRA_STRA", "PWD_PWD")] = "parental strains"
+# 
+# raw$HybridStatus[
+#   raw$Strain %in% c("BUSNA_PWD", "PWD_BUSNA", "STRA_SCHUNT", "SCHUNT_STRA")] = "outbred hybrids"
+# 
+# raw$HybridStatus[
+#   raw$Strain %in% c("BUSNA_STRA", "STRA_BUSNA", "PWD_SCHUNT", "SCHUNT_PWD")] = "inter subsp. hybrids"
+# 
+# write.csv(raw, "../data/1_informationTables/Exp005_WDS_21.6.18.csv", row.names = F)
+
+infoTable = read.csv("../data/1_informationTables/Exp005_WDS_21.6.18.csv")
+
+Nmice = nrow(infoTable)
+
+# Split in 4 groups based on age
+
+
+
+# Give EH_ids
+num = as.numeric(sub("LM", "", firstEH_Id))
+num = num + (1:(Nmice))
+EH_id = paste0("LM", sprintf("%04d", num))
+
+# Spread names randomly among mice
+set.seed(myseed)
+infoTable$EH_id <- sample(EH_id)
+
+# Spread infection equally among sex and Mouse_strain
+table(infoTable$Sex, infoTable$Strain)
+return(infoTable)
+
+
+
+myDesignTable3 <- makeDesignTable(myseed = 1234 ,
                                   pathToInfoTable = "../data/1_informationTables/Exp004_May2018_wildmice_Eferrisi_secondbatch_INFO.csv",
                                   firstEH_Id = "LM0145")
 
@@ -26,7 +66,7 @@ myDesignTable2 <- makeDesignTable(myseed = 1234 ,
 library(experiment)
 
 expe <- randomize(data = myDesignTable2, group = c("E64", "E139"),
-                   indx = myDesignTable2$EH_id, block = myDesignTable2$Strain)
+                  indx = myDesignTable2$EH_id, block = myDesignTable2$Strain)
 
 trt <- data.frame(infection_isolate = expe$treatment)
 trt$EH_id <- rownames(trt)
