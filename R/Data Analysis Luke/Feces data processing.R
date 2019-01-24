71e590e5ae24d7c5c647e051420a07720057c66d
Alice Balard <alice.balard@fu-berlin.de>
2019-01-24 16:35:58 +0100

    Organised Luke's data

--- R/Data Analysis Luke/Feces data processing.R
+++ R/Data Analysis Luke/Feces data processing.R
@@ -1,20 +1,35 @@
-setwd("Repositories/Eimeria_Lab/R/Data Analysis Luke/")
+# setwd("Repositories/Eimeria_Lab/R/Data Analysis Luke/")
+
 #load csv files#
-Inf2a <- read.csv("Inf2a_Exp005.DESIGN.csv")
-Inf2b <- read.csv("Inf2b_Exp005.DESIGN.csv")
-E7aF <- read.csv("Exp_007a feces.csv")
-E7bF <- read.csv("Exp_007b feces.csv")
-#remove uneccessary columns#
-E7aFC <- E7aF[, -c(8:18)]
-E7bFC <- E7bF[, -c(8:18)]
-Inf2aC <- Inf2a[, -c(1:2)]
-Inf2aC <- Inf2aC[, -c(2:25)]
-Inf2aC <- Inf2aC [, -c(3:4)]
-Inf2bC <- Inf2b[, -c(1:2)]
-Inf2bC <- Inf2bC[, -c(2:25)]
-Inf2bC <- Inf2bC [, -c(3:4)]
-#rename EH_id to EH_ID#
-names(Inf2aC)[names(Inf2aC) == "EH_id"] <- "EH_ID"
-names(Inf2bC)[names(Inf2bC) == "EH_id"] <- "EH_ID"
-#merge feces and infection dfs#
-rbind.data.frame(Inf2aC)
+Exp007a_design <- read.csv("../../data/2_designTables/Inf2a_Exp005.DESIGN.csv")
+Exp007b_design <- read.csv("../../data/2_designTables/Inf2b_Exp005.DESIGN.csv")
+E7aF <- read.csv("../../data/3_recordingTables/Exp007/Exp007a/Exp_007a feces.csv")
+E7bF <- read.csv("../../data/3_recordingTables/Exp007/Exp007b/Exp_007b feces.csv")
+
+# the columns we want to keep
+col2keep <- c("Strain", "HybridStatus", "InfectionStrain", "EH_id")
+
+Exp007a_design <- Exp007a_design[col2keep]
+Exp007b_design <- Exp007b_design[col2keep]
+
+# rename EH_id to EH_ID#
+names(Exp007a_design)[names(Exp007a_design) == "EH_id"] <- "EH_ID"
+names(Exp007b_design)[names(Exp007b_design) == "EH_id"] <- "EH_ID"
+
+# let's make one big fat Expe007 design table
+Expe007_design <- rbind(Exp007a_design, Exp007b_design)
+
+# remove shit columns
+E7aF <- E7aF[-grep(pattern = "X", x = names(E7aF))]
+E7bF <- E7bF[-grep(pattern = "X", x = names(E7bF))]
+
+# keep the batch information
+E7aF$batch <- "october2018"
+E7bF$batch <- "december2018"
+
+# Make one big fat table Expe 7
+Expe007_record <- rbind(E7aF, E7bF)
+
+# Merge all
+Expe007 <- merge(Expe007_design, Expe007_record)
+
