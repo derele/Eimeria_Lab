08b332b28b04c1199feb1b63fd6fb8673310e020
Alice Balard <alice.cam.balard@gmail.com>
2019-01-31 16:51:27 +0100

    Update with last day Expe008

--- R/Exp008.R
+++ R/Exp008.R
@@ -16,13 +16,22 @@ Expe008 <- calculateWeightLoss(Expe008, startingDay = 0)
 Expe008$EimeriaSpecies[Expe008$Isolate %in% c("E64", "E139")] <- "E. ferrisi"
 Expe008$EimeriaSpecies[Expe008$Isolate %in% c("E88", "Eflab")] <- "E. falciformis"
 
+# summary (mean and 95%CI)
+Expe008_summary <- na.omit(summarySE(Expe008, measurevar = "relativeWeight",
+                                     groupvars=c("EimeriaSpecies",
+                                                 "Isolate", "dpi"), na.rm = T))
 # Plot follow weight
-ggplot(Expe008, aes(x = dpi, y = relativeWeight)) +
-  geom_line(aes(group = EH_ID, col = Isolate)) +
-  geom_point(size=4, pch = 21, aes(fill = Isolate))+
+ggplot(Expe008_summary, aes(x = dpi, y = relativeWeight)) +
+  geom_line(aes(group = Isolate, col = Isolate)) +
+  geom_errorbar(aes(ymin = relativeWeight - sd,
+                    ymax = relativeWeight + sd, 
+                    col = Isolate),
+                width = .2,
+                position = "dodge") +
+  geom_point(size=2, pch = 21, aes(fill = Isolate))+
   mytheme +
   scale_x_continuous(breaks = 0:11, name = "Day post infection (dpi)") +
   scale_y_continuous(name = "Weight (g)") +
   theme(strip.text.y = element_text(size = 15)) +
-  ggtitle("Weight along experiment per individual") +
+  ggtitle("Expe_008. Weight relative to dpi 0 (mean +/-sd)") +
   facet_grid(.~EimeriaSpecies)
