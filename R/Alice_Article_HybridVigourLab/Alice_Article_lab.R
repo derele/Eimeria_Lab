08546bcdce071018cca8b03bc942b28d59bd5923
Alice Balard <alice.cam.balard@gmail.com>
2019-04-03 18:05:20 +0200

    up +++ stats alice

--- R/Alice_Article_HybridVigourLab/Alice_Article_lab.R
+++ R/Alice_Article_HybridVigourLab/Alice_Article_lab.R
@@ -34,11 +34,32 @@ plotsExpe5[[1]]+ coord_cartesian(ylim = c(-10,20))
 plotsExpe5[[2]]
 
 ## ALL TOGETHER
-# BEFORE cleaning Expe001, we remove it cause weird values, different from all others
+# CHOICE MADE : remove Ploen mice & NMRI. Variations are too high.
+ALL_Expe <- merge(ExpeDF_003_4, ExpeDF_005, all = T)
 
-# ALL_Expe <- merge(ExpeDF_001, ExpeDF_002, all = T)
-ALL_Expe <- merge(ExpeDF_002, ExpeDF_003_4, all = T)
-ALL_Expe <- merge(ALL_Expe, ExpeDF_005, all = T)
+# NB if NA for oocysts.per.tube, set to 0 if the following day as 0 oocysts
+ALL_Expe[is.na(ALL_Expe$oocysts.per.tube), "dpi"] 
+# mainly dpi 1,2 and 3 (not taken) Check if zero at dpi4
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 4]
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 3]
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 2]
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 1]
+miceWithNoOoDPI4 <- ALL_Expe$EH_ID[ALL_Expe$dpi %in% 4 & ALL_Expe$oocysts.per.tube %in% 0]
+ALL_Expe$oocysts.per.tube[ALL_Expe$EH_ID %in% miceWithNoOoDPI4 &
+                            ALL_Expe$dpi %in% 3 & 
+                            is.na(ALL_Expe$oocysts.per.tube)] <- 0
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 4]
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 3]
+# So we are safe to do : 
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 2] <- 0
+ALL_Expe$oocysts.per.tube[ALL_Expe$dpi %in% 1] <- 0
+
+# what's left
+miceDeadBeforeEnd <- c("LM0168", "LM0187", "LM0189", "LM0193")
+ALL_Expe <- ALL_Expe[!ALL_Expe$EH_ID %in% miceDeadBeforeEnd,]
+
+ALL_Expe[is.na(ALL_Expe$oocysts.per.tube), ] 
+# mouse LM0202 has one peak day missing for oocyst collection cause diarrhea
 
 plotsALL <- makeIntermPlots(ALL_Expe)
 plotsALL[[1]] + coord_cartesian(ylim = c(-10,20))
@@ -46,11 +67,8 @@ plotsALL[[2]]# + coord_cartesian(ylim = c(-10,20))
 
 # Part 2. Which is, by experiment, dpimaxOPG / dpimaxweightloss, 
 # by host strain, by parasite isolate
-
-# BEFORE cleaning Expe001, we remove it cause weird values, different from all others
-# toleranceTable <- merge(tolerance_001, tolerance_002, all = T)
-toleranceTable <- merge(tolerance_002, tolerance_003_4, all = T)
-toleranceTable <- merge(toleranceTable, tolerance_005, all = T)
+toleranceTable <- merge(tolerance_003_4, tolerance_005, all = T)
+toleranceTable <- toleranceTable[!toleranceTable$EH_ID %in% miceDeadBeforeEnd,]
 
 toleranceTable$diff_maxWL_maxOPG <- 
   toleranceTable$dpi_maxweightloss - toleranceTable$dpi_maxOPG
@@ -75,15 +93,13 @@ by_host_parasite_peak <- toleranceTable %>%
   data.frame()
 by_host_parasite_peak
 # infection_isolate meanPeakParasite medianPeakParasite
-# 1      E.ferrisi (E139)         6.066667                  6
-# 2       E.ferrisi (E64)         6.142857                  6
-# 3   E.falciformis (E88)         8.266667                  8
-# 4 E.falciformis (EfLab)         8.705882                  9
+# 1    E.ferrisi (E139)         6.125000                  6
+# 2     E.ferrisi (E64)         6.240000                  6
+# 3 E.falciformis (E88)         8.090909                  8
 # sdPeakParasite meanPeakHost medianPeakHost sdPeakHost
-# 1      0.6914918     5.700000              5   3.302559
-# 2      0.4900670     5.057143              5   2.564449
-# 3      0.5208305     7.600000              9   3.529189
-# 4      0.4696682     8.764706              9   2.537947
+# 1      0.7408867     5.250000              5   3.110291
+# 2      0.5174506     4.660000              5   2.615573
+# 3      0.2942449     8.090909              9   3.544400
 
 # Shift all according to median peak, and keep equal window
 mydataShifted <- data.frame(Exp_ID = ALL_Expe$Exp_ID,
@@ -93,31 +109,27 @@ mydataShifted <- data.frame(Exp_ID = ALL_Expe$Exp_ID,
                             dpi = ALL_Expe$dpi,
                             startingWeight = ALL_Expe$startingWeight)
 
-# shift all centered on dpi 9 (centered around Eflab)
+# shift all centered on dpi 8 (centered around E88 parasite peak)
+## oocysts
+O_E88 <- ALL_Expe[ALL_Expe$infection_isolate == "E88", c("dpi", "oocysts.per.tube", "fecweight", "EH_ID")]
+O_E64 <- ALL_Expe[ALL_Expe$infection_isolate == "E64", c("dpi", "oocysts.per.tube", "fecweight", "EH_ID")]
+O_E64$dpi <- O_E64$dpi + 2 
+O_E139 <- ALL_Expe[ALL_Expe$infection_isolate == "E139", c("dpi", "oocysts.per.tube", "fecweight", "EH_ID")]
+O_E139$dpi <- O_E139$dpi + 2 
+
+oocystsShifted <- rbind(O_E88, O_E64, O_E139)
 
 ## weight
-W_Eflab <- ALL_Expe[ALL_Expe$infection_isolate == "EfLab", c("dpi", "weight", "EH_ID")]
 W_E88 <- ALL_Expe[ALL_Expe$infection_isolate == "E88", c("dpi", "weight", "EH_ID")]
+W_E88$dpi <- W_E88$dpi - 1 
 W_E64 <- ALL_Expe[ALL_Expe$infection_isolate == "E64", c("dpi", "weight", "EH_ID")]
-W_E64$dpi <- W_E64$dpi + 4 
+W_E64$dpi <- W_E64$dpi + 3 
 W_E139 <- ALL_Expe[ALL_Expe$infection_isolate == "E139", c("dpi", "weight", "EH_ID")]
-W_E139$dpi <- W_E139$dpi + 4 
+W_E139$dpi <- W_E139$dpi + 3 
 
-weightShifted <- rbind(W_Eflab, W_E88, W_E64, W_E139)
+weightShifted <- rbind(W_E88, W_E64, W_E139)
 
 mydataShifted <- merge(mydataShifted, weightShifted)
-
-## oocysts (TO CORRECT FOR MISSING DATA FRANCI NB)
-O_Eflab <- ALL_Expe[ALL_Expe$infection_isolate == "EfLab", c("dpi", "oocysts.per.tube", "fecweight", "EH_ID")]
-O_E88 <- ALL_Expe[ALL_Expe$infection_isolate == "E88", c("dpi", "oocysts.per.tube", "fecweight", "EH_ID")]
-O_E88$dpi <- O_E88$dpi + 1 
-O_E64 <- ALL_Expe[ALL_Expe$infection_isolate == "E64", c("dpi", "oocysts.per.tube", "fecweight", "EH_ID")]
-O_E64$dpi <- O_E64$dpi + 3 
-O_E139 <- ALL_Expe[ALL_Expe$infection_isolate == "E139", c("dpi", "oocysts.per.tube", "fecweight", "EH_ID")]
-O_E139$dpi <- O_E139$dpi + 3 
-
-oocystsShifted <- rbind(O_Eflab, O_E88, O_E64, O_E139)
-
 mydataShifted <- merge(mydataShifted, oocystsShifted)
 
 ## restrict window to get same info on all
@@ -125,8 +137,134 @@ table(oocystsShifted$dpi)
 table(weightShifted$dpi)
 table(ALL_Expe$dpi)
 table(mydataShifted$dpi)
+table(mydataShifted$dpi, mydataShifted$fecweight == 0)
 
-mydataShifted <- mydataShifted[mydataShifted$dpi >= 4,]
+#### Keep WINDOWS OF INFECTION #### 
+# peak = dpi 8. 3 days before + peak + 2 days after full mice
+mydataShifted <- mydataShifted[mydataShifted$dpi %in% 5:10,]
+table(mydataShifted$dpi)
+
+# consider weightBegWind as weight beginning of window
+A = mydataShifted[mydataShifted$dpi == 5, c("weight", "EH_ID")]
+names(A)[1] = "weightBegWind"
+mydataShifted = merge(mydataShifted, A)
+# Cumulative sum of our weight loss
+mydataShifted = mydataShifted %>% 
+  group_by(EH_ID) %>% 
+  dplyr::arrange(dpi, .by_group = TRUE) %>%
+  dplyr::mutate(relativeWeight = weight / weightBegWind * 100)
+mydataShifted = data.frame(mydataShifted)
+
+mydataShifted$OPG <- mydataShifted$oocysts.per.tube / mydataShifted$fecweight
+
+# Summary DF within infection window
+
+### FROM THERE ON
+All_summary_windows <- mydataShifted %>% 
+  group_by(EH_ID) %>% 
+  summarise(minWeight = min(weight),
+            maxOo = max(OPG))
+All_summary_windows
+mydataShifted$weightBegWind
+
+X <- mydataShifted %>% 
+  group_by(EH_ID) %>%
+  slice(which.min(weight)) 
+
+X
+X[c("EH_ID", "weight", "weightBegWind")]
+
+  filter(weight == min(weight))# %>% # filter the data.frame to keep row where x is maximum
+  select(dpi) # select column y
+
+
+# dplyr::arrange(dpi, .by_group = TRUE) %>%
+  dplyr::mutate(m = weight / weightBegWind * 100)
+
+mydataShifted$weight
+
+max.loss <- do.call("rbind", by(mydataShifted, mydataShifted$EH_ID, function (x){
+  m.loss <- which(mydataShifted$weight == min(mydataShifted$weight, na.rm=TRUE))
+  x[m.loss,]
+}))
+max.loss <- max.loss[!duplicated(max.loss$EH_ID),]
+names(max.loss)[names(max.loss) %in% "dpi"] <- "dpi_maxweightloss"
+names(max.loss)[names(max.loss) %in% "weight"] <- "minRelativeWeight"
+max.loss
+
+
+
+# create a table with tolerance factor, max.loss, max.OPG and sum.oocysts concatenated
+# makeToleranceTable <- function(ExpeDF,
+#                                dayVar = c("dpi", "mean_Neubauer", "OPG", "labels", "Neubauer1",
+#                                           "Neubauer2", "Neubauer3", "Neubauer4", "dilution_ml", "weight",                   
+#                                           "weightloss", "weightRelativeToInfection", "fecweight", 
+#                                           "comment.x", "comment.y", "X.x", "relativeWeight", 
+#                                           "X", "X.1", "X.2", "X.3", "X.4", "MM",
+#                                           "oocysts.per.tube", "label")){
+  # maximum weightloss
+  max.loss = getMaxLoss(ExpeDF)
+  max.loss = max.loss[!names(max.loss) %in% dayVar]
+  # max and sum of oocysts
+  max.OPG = getMaxOPG(ExpeDF)
+  max.OPG = max.OPG[!names(max.OPG) %in% dayVar]
+  # sum
+  sum.oocysts = getSumOPG(ExpeDF)
+  sum.oocysts = sum.oocysts[!names(sum.oocysts) %in% dayVar]
+  # tolerance
+  tolerance = merge(max.loss, max.OPG)
+  tolerance = merge(tolerance, sum.oocysts)
+  # maxweightloss
+  tolerance$maxweightloss = 100 - tolerance$minRelativeWeight
+  # REMOVE UNINFECTED ANIMALS
+  tolerance <- tolerance[tolerance$maxOPG_inmillion != 0,]
+  # http://science.sciencemag.org/content/318/5851/812
+  # TO CORRECT LATER
+  # !!!!!
+  # resistance (inverse of peak parasite density) 
+  # tolerance (slope of a regression of minimum weight retained against peak parasite load)
+  tol = -(tolerance$maxweightloss / tolerance$maxOPG_inmillion)
+  tolerance$toleranceFactor = tol + abs(min(tol))
+  # erase useless level (Eflab)
+  tolerance$infection_isolate <- droplevels(tolerance$infection_isolate)
+  # NB. Let's not consider which parent is which, but make A_B mouse = B_A mouse
+  # we don't have enough individuals to test this effect, and we are not interested in it anyway!
+  x <- strsplit(tolerance$Mouse_strain, "_")
+  y <- lapply(x, sort)
+  z <- unlist(lapply(y, FUN = function(x){paste(x, collapse="-")}))
+  tolerance$Mouse_genotype <- z
+  ## Order the levels to be more clear in later plots (parents will be low and down 
+  ## on the legend, hybrids in between...)
+  tolerance$Mouse_genotype <- factor(tolerance$Mouse_genotype,
+                                     levels = c("NMRI", 
+                                                "WSB", "WP", "PWD1",
+                                                "BUSNA-BUSNA", "PWD-PWD",
+                                                "BUSNA-PWD",
+                                                "BUSNA-STRA","PWD-SCHUNT",
+                                                "SCHUNT-STRA",
+                                                "SCHUNT-SCHUNT","STRA-STRA"),
+                                     labels = c("NMRI", 
+                                                "MMd_F0 (Ws-Ws)", "Mmm-Mmd_Hybrid (WP)", "MMm_F0 (Pw1-Pw1)",
+                                                "MMm_F0 (Bu-Bu)",
+                                                "MMm_F0 (Pw-Pw)",
+                                                "MMm_F1 (Bu-Pw)",
+                                                "Mmm-Mmd_F1 (Bu-St)",
+                                                "Mmm-Mmd_F1 (Pw-Sc)",
+                                                "MMd_F1 (Sc-St)",
+                                                "MMd_F0 (Sc-Sc)",
+                                                "MMd_F0 (St-St)"))
+  tolerance$infection_isolate <- factor(tolerance$infection_isolate,
+                                        levels = c("E139", "E64", "E88", "EfLab"),
+                                        labels = c("E.ferrisi (E139)",
+                                                   "E.ferrisi (E64)",
+                                                   "E.falciformis (E88)",
+                                                   "E.falciformis (EfLab)"))
+  return(tolerance)
+# }
+
+
+
+
 
 ## Statistical models along dpi
 
@@ -134,7 +272,7 @@ mydataShifted <- mydataShifted[mydataShifted$dpi >= 4,]
 #https://stats.stackexchange.com/questions/237963/how-to-formulate-the-offset-of-a-glm
 
 ### Which distribution?
-hist(mydataShifted$weight) # not normal if NMRI are included, bimodal. Can we approx Normal?
+hist(mydataShifted$weight)
 hist(mydataShifted$oocysts.per.tube, breaks = 100) # clearly negbin here :) 
 
 x <- as.numeric(na.omit(mydataShifted$oocysts.per.tube))
@@ -184,52 +322,81 @@ ggplot(mydataShifted, aes(x = dpi, y = weight, group = dpi)) +
 # test significance of interactions
 # Original model
 model1 <- glmer.nb(oocysts.per.tube.tsd ~ infection_isolate * Mouse_strain + 
-                    (1|dpi) + offset(log(fecweight)), 
-                  data = mydataShifted)
+                     (1|dpi) + offset(log(fecweight)),
+                   data = mydataShifted[mydataShifted$fecweight != 0,])
+
 # Model without an interaction
 model2 <- glmer.nb(oocysts.per.tube.tsd ~ infection_isolate + Mouse_strain + 
                      (1|dpi) + offset(log(fecweight)), 
-                   data = mydataShifted)
+                   data = mydataShifted[mydataShifted$fecweight != 0,])
+
 # Likelihood ratio test
 anova(model1, model2, test="LRT")
-## --> INTERACTIONS NON SIGNIFICANT
-summary(model2)
+## --> INTERACTIONS SLIGHTLY SIGNIFICANT
+summary(model1)
 
 library(sjPlot)
-sjPlot::plot_model(model2, sort.est = T, show.values = TRUE, value.offset = .3)
+# I don't get that plot
+# sjPlot::plot_model(model1, sort.est = T, show.values = TRUE, value.offset = .3)
 
 # Post-hoc analysis
-
 if(!require(multcomp)){install.packages("multcomp")}
 library(multcomp)
-s1 <- summary(glht(model2, mcp(infection_isolate="Tukey")))
-s2 <- summary(glht(model2, mcp(Mouse_strain="Tukey")))
-s1
-s2
+s1 <- summary(glht(model1, mcp(infection_isolate="Tukey")))
+# Linear Hypotheses:
+#   Estimate Std. Error z value Pr(>|z|)   
+# E139 - E64 == 0  -1.0111     0.2997  -3.373  0.00207 **
+#   E88 - E64 == 0    0.4042     0.4589   0.881  0.64587   
+# E88 - E139 == 0   1.4153     0.4801   2.948  0.00860 **
+s2 <- summary(glht(model1, mcp(Mouse_strain="Tukey")))
+# only significant : 
+# SCHUNT_SCHUNT - PWD_PWD == 0     -1.05978    0.27068  -3.915    <0.01 **
+
+# and summary (sum of oocysts during window)
+
+
 ### Host suffering
 
 # test significance of interactions
 # Original model
+mydataShifted[is.na(mydataShifted$weight),]
+
 model1.w <- lmer(weight ~ infection_isolate * Mouse_strain + 
-                   (1|dpi) + offset(log(startingWeight)), 
+                   (1|dpi) + offset(log(weightBegWind)), 
                  data = mydataShifted)
 # Model without an interaction
 model2.w <- lmer(weight ~ infection_isolate + Mouse_strain + 
-                    (1|dpi) + offset(log(startingWeight)), 
+                    (1|dpi) + offset(log(weightBegWind)), 
                   data = mydataShifted)
 # Likelihood ratio test
 anova(model1.w, model2.w, test="LRT")
 
-## --> INTERACTIONS SIGNIFICANT
+## --> INTERACTIONS SIGNIFICANT +++ p-value 4.194e-10 ***
 summary(model1.w)
 
 # Post-hoc analysis
 if(!require(multcomp)){install.packages("multcomp")}
 library(multcomp)
 s.P.w <- summary(glht(model1.w, mcp(infection_isolate="Tukey")))
-s.P.w
+s.P.w # NOPE
 s.H.w <- summary(glht(model1.w, mcp(Mouse_strain="Tukey")))
-s.H.w
+s.H.w # LOADS OF DIFFERENCES!!
+
+### SUMMARY OVER EXPE (to compare)
+
+
+m1 = lm(response ~ group * infection_isolate,
+        data=mydata)
+mytukey = TukeyHSD.lm(m1)
+mytukeyDF = lapply(mytukey, function(x) {
+  df <- data.frame(x)
+  data.frame(combi = rownames(df[df$p.adj < 0.05,]),
+             padj = df$p.adj[df$p.adj < 0.05],
+             diff = df$p.adj[df$p.adj < 0.05])})
+return(list(model = m1,
+            summary.model = summary(m1),
+            mytukeyHSD = mytukeyDF))
+
 
 ### Tolerance (slope) Raberg 2007 Raber 2008
 
