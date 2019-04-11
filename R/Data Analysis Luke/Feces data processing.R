f52c69aacc39968afdc526255ec87ace4f20e2ea
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-04-11 16:46:30 +0200

    minor convenience rewrite

--- R/Data Analysis Luke/Feces data processing.R
+++ R/Data Analysis Luke/Feces data processing.R
@@ -2,11 +2,11 @@
 
 library(Rmisc)
 
-#load csv files#
-Exp007a_design <- read.csv("../../data/2_designTables/Exp007a_design.csv")
-Exp007b_design <- read.csv("../../data/2_designTables/Exp007b_design.csv")
-E7aF <- read.csv("../../data/3_recordingTables/Exp007/Exp007a/Exp_007a_feces.csv")
-E7bF <- read.csv("../../data/3_recordingTables/Exp007/Exp007b/Exp_007b_feces.csv")
+#load csv files at HU#
+Exp007a_design <- read.csv("../luke/Repositories/Eimeria_Lab/data/2_designTables/Exp007a_design.csv")
+Exp007b_design <- read.csv("../luke/Repositories/Eimeria_Lab/data/2_designTables/Exp007b_design.csv")
+E7aF <- read.csv("../luke/Repositories/Eimeria_Lab/data/3_recordingTables/Exp007/Exp007a/Exp_007a_feces.csv")
+E7bF <- read.csv("../luke/Repositories/Eimeria_Lab/data/3_recordingTables/Exp007/Exp007b/Exp_007b_feces.csv")
 
 # the columns we want to keep
 col2keep <- c("Strain", "HybridStatus", "InfectionStrain", "EH_ID")
@@ -15,8 +15,8 @@ Exp007a_design <- Exp007a_design[col2keep]
 Exp007b_design <- Exp007b_design[col2keep]
 
 # rename EH_id to EH_ID#
-#names(Exp007a_design)[names(Exp007a_design) == "EH_id"] <- "EH_ID"
-#names(Exp007b_design)[names(Exp007b_design) == "EH_id"] <- "EH_ID"
+names(Exp007a_design)[names(Exp007a_design) == "EH_id"] <- "EH_ID"
+names(Exp007b_design)[names(Exp007b_design) == "EH_id"] <- "EH_ID"
 
 # let's make one big fat Expe007 design table (E88 = 31 entries, E64 = 38 entries)
 Exp007_design <- rbind(Exp007a_design, Exp007b_design)
@@ -47,18 +47,18 @@ str(Exp007_E88)
 Exp007_E88[,9] <- sapply(Exp007_E88[,9], as.numeric)
 
 
-# calculate summary statistics on weight loss#
-WLoss_007_E64 <- summarySE(Exp007_E64, measurevar = "Wchange",
-                                   groupvars=c("HybridStatus", "dpi", "batch"), na.rm = T)
-WLoss_007_E64$ci[is.na(WLoss_007_E64$ci)] <- 0
-
-WLoss_007_E88 <- summarySE(Exp007_E88, measurevar = "Wchange",
-                                   groupvars=c("HybridStatus", "dpi", "batch"), na.rm = T)
-WLoss_007_E88$ci[is.na(WLoss_007_E88$ci)] <- 0
+# calculate summary statistics on weight loss NEEDS FIXING #
+#WLoss_007_E64 <- summarise(Exp007_E64, measurevar = "Wchange",
+#                                   groupvars=c("HybridStatus", "dpi", "batch"), na.rm = T)
+#WLoss_007_E64$ci[is.na(WLoss_007_E64$ci)] <- 0
+#
+#WLoss_007_E88 <- summarySE(Exp007_E88, measurevar = "Wchange",
+#                                   groupvars=c("HybridStatus", "dpi", "batch"), na.rm = T)
+#WLoss_007_E88$ci[is.na(WLoss_007_E88$ci)] <- 0
 
 #convert Exp007 Wchange to numeric, replace dpi0 0% with 100%#
-Exp007[,9] <- sapply(Exp007[,9], as.numeric)
-Exp007$Wchange[Exp007$Wchange == 0] <- 100
+#Exp007[,9] <- sapply(Exp007[,9], as.numeric)
+#Exp007$Wchange[Exp007$Wchange == 0] <- 100
 
 #Summary and confidence intervals (feces only)#
 library(dplyr)
