1d79b0050e7d044898810eae3fac7fd380c7a62e
Luke <lubomr.luke.bednar@gmail.com>
2019-05-06 11:49:51 +0200

    organize and save

---
+++ R/Data Analysis Luke/qPCR_Jan2017.R
@@ -0,0 +1,27 @@
+#install.packages("RDML")
+#install.packages("xml2")
+end_point <- read.csv("../lubomir/Documents/Eimeria_Lab/Jan2017/Caecum_RT-qPCR/18.04.2019/FAM=SYBR/Luke_2019-04-18 15-46-21_CC011384 -  End Point Results_FAM.csv", sep = ";")
+melt_deriv <- read.csv("../lubomir/Documents/Eimeria_Lab/Jan2017/Caecum_RT-qPCR/18.04.2019/FAM=SYBR/Luke_2019-04-18 15-46-21_CC011384 -  Melt Curve Derivative Results_FAM.csv", sep = ";")
+melt_RFU <- read.csv("../lubomir/Documents/Eimeria_Lab/Jan2017/Caecum_RT-qPCR/18.04.2019/FAM=SYBR/Luke_2019-04-18 15-46-21_CC011384 -  Melt Curve RFU Results_FAM.csv", sep = ";")
+quant_amp <- read.csv("../lubomir/Documents/Eimeria_Lab/Jan2017/Caecum_RT-qPCR/18.04.2019/FAM=SYBR/Luke_2019-04-18 15-46-21_CC011384 -  Quantification Amplification Results_FAM.csv", sep = ";")
+#check structure
+str(end_point)
+str(melt_deriv)
+str(melt_RFU)
+str(quant_amp)
+#convert factors to numeric, the "," from original .csv is the problem
+end_point <- data.frame(lapply(end_point, function(x) {gsub(",", ".", x)}))
+str(end_point)
+end_point[,'End.RFU'] <- as.numeric(as.character(end_point[,'End.RFU']))
+
+melt_deriv <- data.frame(lapply(melt_deriv, function(x) {gsub(",", ".", x)}))
+str(melt_deriv)
+
+melt_RFU <- data.frame(lapply(melt_RFU, function(x) {gsub(",", ".", x)}))
+str(melt_RFU)
+
+quant_amp <- data.frame(lapply(quant_amp, function(x) {gsub(",", ".", x)}))
+str(quant_amp)
+
+#indx <- sapply(end_point$End.RFU, is.factor)
+#end_point[indx] <- lapply(end_point[indx], function(y) as.numeric(as.character(y)))
