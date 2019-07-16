e26cf759a6fc33311340784f3ec7da80bfeb39eb
Luke <lubomr.luke.bednar@gmail.com>
2019-07-16 16:29:10 +0200

    Fix folder naming + missing labels from RT-qPCR

--- R/Data Analysis Luke/E1_012017_Eim_raw_RT-qPCR_celanup.R
+++ R/Data Analysis Luke/E1_012017_Eim_raw_RT-qPCR_celanup.R
@@ -4,3 +4,11 @@ library(httr)
 library(Rmisc)
 
 #load in data from GitHub
+Samples36_37_38_39url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/20_05_2019_Samples_36_37_38_39/Luke_2019_05_21_Enas2017samples_36_37_38_39%20-%20%20Quantification%20Cq%20Results_0.csv"
+Samples36_37_38_39 <- read.csv(text = getURL(Samples36_37_38_39url), sep = ";")
+
+Samples_C_27_74_95url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/20_05_2019_Samples_C_27_74_95/admin_2019-05-20%2011-56-24_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
+Samples_C_27_74_95 <- read.csv(text = getURL(Samples_C_27_74_95url), sep = ";")
+
+Samples_40_41_43_46url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/20_05_2019_Samples_C_27_74_95/admin_2019-05-20%2011-56-24_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
+Samples_40_41_43_46 <- read.csv(text = getURL(Samples_40_41_43_46url), sep = ";")
