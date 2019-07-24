e0d38e1c25b84eb2c69d01c3a4626ff83e86cb77
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-07-24 20:33:51 +0200

    Fix E1_RT-qPCR double labels, rewrite script to accomodate

--- R/Data Analysis Luke/E1_012017_Eim_RT-qPCR_Analysis.R
+++ R/Data Analysis Luke/E1_012017_Eim_RT-qPCR_Analysis.R
@@ -3,17 +3,44 @@ library(RCurl)
 library(httr)
 library(Rmisc)
 library(ggplot2)
+library(dplyr)
+library(compare)
+library(viridis)
+library(RColorBrewer)
 
 #load in data from GitHub
 RTqPCRurl <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/b4efd8df335199ff9037634c5ba1d909a7d58baa/data/3_recordingTables/E1_012017_Eim_RT-qPCR_clean.csv"
 RTqPCR <- read.csv(text = getURL(RTqPCRurl))
-#RTqPCR <- read.csv(file = "./Documents/Eimeria_Lab/data/3_recordingTables/E1_012017_Eim_RT-qPCR_clean.csv")
+# manual loading: 
+RTqPCR <- read.csv(file = "./Eimeria_Lab/data/3_recordingTables/E1_012017_Eim_RT-qPCR_clean.csv", sep = ";")
+#change colnames to match standard
+names(RTqPCR)[names(RTqPCR) == "Sample"] <- "EH_ID"
+#average duplicates and add SD
+AVG <- RTqPCR %>% group_by(Target, EH_ID) %>% 
+  summarize(SD = sd(Cq.Mean),
+            Cq.Mean = mean(Cq.Mean))
 
-#very minor cleanup
-RTqPCR$X <- NULL
-RTqPCR$Cq <- NULL
+#add mouse data
+InfectionURL <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/2_designTables/E1_012017_Eim_Experiment_Table_raw_NMRI.csv"
+Infection <- read.csv(text = getURL(InfectionURL))
+#rename columns and merge
+names(Infection)[names(Infection) == "mouseID"] <- "EH_ID"
+names(Infection)[names(Infection) == "inf.strain"] <- "InfectionStrain"
+AVG_INF <- merge(AVG, Infection, by = "EH_ID", all = TRUE)
+#missing 80 values, compare
+comparison <- compare(AVG,AVG_INF,allowAll=TRUE)
+comparison$tM
+#write out AVG_inf to correct based on lab book notes
+write.csv(AVG_INF, file = "./AVG_INF.csv")
 
-#graph it out to know what I'm dealing with 
-ggplot(data = RTqPCR, aes(x = Sample, y = Cq.Mean, color = Target)) +
-  geom_boxplot()
+#subset for graphing 
+Inf.groups <- subset(AVG_INF, 
+                InfectionStrain%in%"Eflab"|
+                InfectionStrain%in%"Efwild"|
+                InfectionStrain%in%"EI70"|
+                InfectionStrain%in%"EI64"|
+                InfectionStrain%in%"Uninf")
 
+ggplot(AVG_INF, aes(x = InfectionStrain, y = Cq.Mean, color = Target)) +
+  geom_boxplot() +
+  scale_color_viridis(option = "A")
--- R/Data Analysis Luke/E1_012017_Eim_raw_RT-qPCR_celanup.R
+++ R/Data Analysis Luke/E1_012017_Eim_raw_RT-qPCR_celanup.R
@@ -13,8 +13,8 @@ Samples_23_24_25_26 <- read.csv(text = getURL(Samples_23_24_25_26url), sep = ";"
 Samples_C_27_74_95url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/20_05_2019_Samples_C_27_74_95/admin_2019-05-20%2011-56-24_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
 Samples_C_27_74_95 <- read.csv(text = getURL(Samples_C_27_74_95url), sep = ";")
 
-Samples_29a_29b_54a_54burl<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/08_07_2019_Samples_29a_29b_54a_54b/admin_2019-07-08%2010-09-26_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
-Samples_29a_29b_54a_54b <- read.csv(text = getURL(Samples_29a_29b_54a_54burl), sep = ";")
+Samples_30_29_54_92url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/08_07_2019_Samples_30_29_54_92/admin_2019-07-08%2010-09-26_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
+Samples_30_29_54_92 <- read.csv(text = getURL(Samples_30_29_54_92url), sep = ";")
 
 Samples_40_41_43_46url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/27_05_2019_Samples_40_41_43_46/admin_2019-05-27%2011-30-32_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
 Samples_40_41_43_46 <- read.csv(text = getURL(Samples_40_41_43_46url), sep = ";")
@@ -31,8 +31,8 @@ Samples_51_55_56_58 <- read.csv(text = getURL(Samples_51_55_56_58url), sep = ";"
 Samples_59_60_61_62url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/29_05_2019_Samples_59_60_61_62/admin_2019-05-29%2010-26-36_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
 Samples_59_60_61_62 <- read.csv(text = getURL(Samples_59_60_61_62url), sep = ";")
 
-Samples_57a_57b_76a_76burl<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/08_07_2019_Samples_57a_57b_76a_76b/admin_2019-07-08%2011-57-32_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
-Samples_57a_57b_76a_76b <- read.csv(text = getURL(Samples_57a_57b_76a_76burl), sep = ";")
+Samples_57_94_70_76url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/08_07_2019_Samples_57_94_70_76/admin_2019-07-08%2011-57-32_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
+Samples_57_94_70_76 <- read.csv(text = getURL(Samples_57_94_70_76url), sep = ";")
 
 Samples_63_64_65_66url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/29_05_2019_Samples_63_64_65_66/admin_2019-05-29%2014-29-53_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
 Samples_63_64_65_66 <- read.csv(text = getURL(Samples_63_64_65_66url), sep = ";")
@@ -52,8 +52,8 @@ Samples_81_82_83_84 <- read.csv(text = getURL(Samples_81_82_83_84url), sep = ";"
 Samples_85_86_88_89url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/02_07_2019_Samples_85_86_88_89/admin_2019-07-02%2011-00-22_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
 Samples_85_86_88_89 <- read.csv(text = getURL(Samples_85_86_88_89url), sep = ";")
 
-Samples_90_91_93_22aurl<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/03_07_2019_Samples_90_91_93_22a/admin_2019-07-03%2010-38-19_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
-Samples_90_91_93_22a <- read.csv(text = getURL(Samples_90_91_93_22aurl), sep = ";")
+Samples_90_91_93_22url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/03_07_2019_Samples_90_91_93_22/admin_2019-07-03%2010-38-19_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
+Samples_90_91_93_22 <- read.csv(text = getURL(Samples_90_91_93_22url), sep = ";")
 
 Samples_87_73_53_31url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E1_012017_Eim_RT-Caecum_qPCR/10_07_2019_%20Samples_87_73_53_31/admin_2019-07-10%2013-29-23_CC011384%20-%20%20Quantification%20Cq%20Results_0.csv"
 Samples_87_73_53_31 <- read.csv(text = getURL(Samples_87_73_53_31url), sep = ";")
@@ -62,9 +62,10 @@ Samples_48url<- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/dat
 Samples_48 <- read.csv(text = getURL(Samples_48url), sep = ";")
 
 #meger it all
-All <- rbind(Samples_48, Samples_49_32_28_34, Samples_51_55_56_58, Samples_57a_57b_76a_76b, Samples_59_60_61_62, Samples_63_64_65_66,
-      Samples_67_68_69_73, Samples_75_77_79_80, Samples_81_82_83_84, Samples_85_86_88_89, Samples_87_73_53_31, 
-      Samples_90_91_93_22a, Samples_C_27_74_95)
+All <- rbind(Samples_48, Samples_87_73_53_31, Samples_90_91_93_22, Samples_85_86_88_89, Samples_81_82_83_84, Samples_75_77_79_80, 
+             Samples_45_71_72_78, Samples_67_68_69_73, Samples_63_64_65_66, Samples_57_94_70_76, Samples_59_60_61_62, Samples_51_55_56_58,
+             Samples_49_32_28_34, Samples_42_44_47_50, Samples_40_41_43_46, Samples_30_29_54_92, Samples_C_27_74_95, Samples_23_24_25_26,
+             Samples_36_37_38_39)
 #Delete rubbish columns
 All$X <- NULL
 All$Biological.Set.Name <- NULL
@@ -73,6 +74,7 @@ All$Starting.Quantity..SQ. <- NULL
 All$Log.Starting.Quantity <- NULL
 All$Well.Note <- NULL
 All$SQ.Mean <- NULL
+All$Cq <- NULL
 All$SQ.Std..Dev <- NULL
 #convert "n.def" and 0 to NAs, Unknowns to Cecum, Empty Target to Nothing and Ppia to Pos Ctrl
 All[All=="n. def."] <- NA
@@ -86,7 +88,11 @@ All <- within(All, Content[Target=="Ppia"] <- ("Pos Ctrl"[Target=="Ppia"]))
 All$Content[is.na(x = All$Content)] <- "Pos Ctrl"
 #Omit NAs
 All <- na.omit(All)
-#write out to new file (Laptop)
-write.csv(x = All, file = "//home/lubomir/Documents/Eimeria_Lab/data/3_recordingTables/E1_012017_Eim_RT-qPCR_clean.csv")
-#write out to new file (Win)
-write.csv(All, "./Eimeria_Lab/data/3_recordingTables/E1_012017_Eim_RT-qPCR_clean.csv", row.names = FALSE)
+#add 00 to LM names
+All$Sample <- sub("LM", "LM00", All$Sample )
+#replace commas with dots for decimal values
+All$Cq.Mean <- gsub(",", '.', All$Cq.Mean, fixed = T)
+#convert number values from factor to numeric
+All$Cq.Mean <- as.numeric(All$Cq.Mean)
+#problems with reading off Git, use write table
+write.table(All, file = "./Eimeria_Lab/data/3_recordingTables/E1_012017_Eim_RT-qPCR_clean.csv", row.names = FALSE, dec = ".", sep = ";", quote = FALSE)
