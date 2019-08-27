4e4bc9a3f9034b981f038dc1661abde1ffb0265b
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-08-27 20:31:56 +0200

    E5 extra table with Wchange + graphing

---
+++ R/Data Analysis Luke/E5-6_062018_Eim_graphing.R
@@ -0,0 +1,31 @@
+library(RCurl)
+library(httr)
+library(Rmisc)
+library(magrittr)
+library(ggplot2)
+
+# load in data
+E6_completeURL <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E6_062018_Eim_complete.csv"
+E6_complete <- read.csv(text = getURL(E6_completeURL), sep = ";", dec = ",")
+
+#convert number columns
+cols = c(4, 5, 6, 7, 8, 10, 12, 13, 14, 15)
+E6_complete[,cols] %<>% lapply(function(x) as.numeric(as.character(x)))
+
+#graph
+
+ggplot(E6_complete, aes(dpi, weight, color=Eimeria_species)) +
+  # geom_jitter(width=0.2) +
+  geom_smooth(se=FALSE) +
+  # scale_x_continuous(breaks=c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11) +
+  #                    labels=c("0" ,"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11")) +
+  facet_wrap(~HybridStatus, scales="free_y", nrow=2) +
+  scale_colour_brewer("infection\nspecies", palette = "Dark2") +
+  scale_y_continuous("weight (g)") +
+  theme_bw()
+
+
+
+Cytokines <- ggarrange(CytokinesSP, CytokinesCE, nrow=2, common.legend = TRUE,
+                       legend="right", labels = "AUTO")
+dev.off()
