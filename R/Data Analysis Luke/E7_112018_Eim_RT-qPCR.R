e958064b7ad3bfd6edf1404b47ec60386acef91a
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-11-19 15:51:45 +0100

    upsate E7 code before HZ18 update

--- R/Data Analysis Luke/E7_112018_Eim_RT-qPCR.R
+++ R/Data Analysis Luke/E7_112018_Eim_RT-qPCR.R
@@ -8,6 +8,8 @@ library(reshape2)
 # load in raw tables
 RT1 <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E7_112018_Eim_RT-qPCRs/E7_112018_Eim_RT-qPCR1/E7_112018_Eim_RT-qPCR1.csv"
 RT1 <- read.csv(text = getURL(RT1))
+#remove LM_0231 because we have a repeat in RT6
+RT1 <- RT1[!grepl("LM_0231", RT1$Name),] 
 
 RT2 <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E7_112018_Eim_RT-qPCRs/E7_112018_Eim_RT-qPCR2/E7_112018_Eim_RT-qPCR2.CSV"
 RT2 <- read.csv(text = getURL(RT2))
@@ -54,12 +56,21 @@ RT <- bind_rows(RT, RT3)
 RT <- bind_rows(RT, RT4)
 RT <- bind_rows(RT, RT5)
 RT <- bind_rows(RT, RT6)
-#rename targets properly
+#remove negative controls
 RT <- RT[!grepl("IRG6A", RT$Name),]
 RT <- RT[!grepl("CXCR3", RT$Name),]
 RT <- RT[!grepl("IL-12rb1", RT$Name),]
 RT <- RT[!grepl("B-actin", RT$Name),]
 RT <- RT[!grepl("GAPDH", RT$Name),]
+# load in MC analysis of RT
+RTMC <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E7_112018_Eim_RT-qPCRs/E7_RT-qPCR_MC.csv"
+RTMC <- read.csv(sep = ";", text = getURL(RTMC))
+names(RTMC)[names(RTMC) == "EH_ID"] <- "Name"
+names(RTMC)[names(RTMC) == "Target"] <- "Target.SYBR"
+RT <- merge(RT, RTMC)
+RT <- RT[!(RT$Caecum == "neg"),]
+RT$Observer <- NULL
+RT$Date <- NULL 
 # name columns to match other data sets (Mouse_ID, Target) + make RT.CT numeric
 names(RT)[names(RT) == "Target.SYBR"] <- "Target"
 names(RT)[names(RT) == "Ct.SYBR"] <- "RT.Ct"
@@ -67,13 +78,13 @@ names(RT)[names(RT) == "Name"] <- "Mouse_ID"
 RT$RT.Ct <- as.numeric(as.character(RT$RT.Ct))
 # remove NAs, calculate averages + save long
 RT <- na.omit(RT)
-RT.wide <- reshape(RT[, c("Target", "Mouse_ID","RT.Ct")],
-                   timevar = "Target", idvar = "Mouse_ID", direction = "wide")
 RT.long <- RT %>% dplyr::group_by(Mouse_ID, Target) %>% dplyr::summarise(RT.Ct = mean(RT.Ct))
 RT.long <- data.frame(RT.long)
+RT.wide <- reshape(RT.long[, c("Target", "Mouse_ID","RT.Ct")],
+                   timevar = "Target", idvar = "Mouse_ID", direction = "wide")
 # set ref and target genes
 refGenes <- c("RT.Ct.B-actin", "RT.Ct.GAPDH")
-targetGenes <- c("RT.Ct.CXCR3", "RT.Ct.IL.12", "RT.Ct.IRG6")
+targetGenes <- c("RT.Ct.CXCR3", "RT.Ct.IL-12", "RT.Ct.IRG6")
 # calculate ref genes in new column and subtract targets from HKG average, create new columns
 RT.wide <- RT.wide %>% mutate(refMean = rowMeans(na.rm = TRUE,select(., refGenes)))
 RT.wide <- data.frame(RT.wide)
@@ -117,10 +128,10 @@ complete$X <- NULL
 complete$EH_ID <- gsub("LM", "LM_", complete$EH_ID)
 complete <- merge(complete, RT.long, by = "EH_ID")
 #quick basic subtract before merge (based on E7 melting curves)
-MC <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E7_112018_Eim_detection_MC.csv"
-MC <- read.csv(text = getURL(MC))
-complete <- merge(complete, MC, by = "EH_ID")
-complete <-complete[!(complete$Caecum == "neg"),]
+E7EimMC <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E7_112018_Eim_detection_MC.csv"
+E7EimMC <- read.csv(text = getURL(E7EimMC))
+complete <- merge(complete, E7EimMC, by = "EH_ID")
+complete <- complete[!(complete$Caecum == "neg"),]
 # add intensity 
 E7_inf <- "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E7_112018_Eim_Anna_qPCR_DNA_ct_Zusammenfassung.csv"
 E7_inf <- read.csv(text = getURL(E7_inf))
@@ -151,6 +162,11 @@ ggplot(complete, aes(x = NE, y = delta, color = Target)) +
 # load in wild data for comparison
 HZ18 <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/Gene_expression/HZ18_RT-qPCR_RTlong.csv"
 HZ18 <- read.csv(text = getURL(HZ18))
+
+
+
+
+
 #process to graph together
 HZ18 <- HZ18[!(HZ18$Target=="GBP2"),]
 HZ18 <- HZ18[!(HZ18$Target=="IL-6"),]
@@ -161,7 +177,7 @@ HZ18$inf <- NULL
 HZ18$HI <- NULL
 
 E7 <- merge(RT.long, E7_inf)
-E7 <- merge(E7, MC, by = "EH_ID")
+E7 <- merge(E7, E7EimMC, by = "EH_ID")
 E7 <- E7[!(E7$Caecum == "neg"),]
 names(HZ18)[names(HZ18) == "deltaCtMmE_tissue"] <- "delta"
 E7 %>% mutate_if(is.factor, as.character) -> E7
