a76c52b91e9d5fec38a448459fdd9cb8d2a24ba1
LubomirBednar <lubomir.luke.bednar@gmail.com>
2020-02-26 14:13:58 +0100

    add E6 processing script for weight and oocysts + generate a complete .csv

--- R/Data Analysis Luke/E5_062018_Eim_weight.R
+++
@@ -1,9 +0,0 @@
-# process E6 weight and oocysts
-library(tidyverse)
-library(ggplot2)
-library(Rmisc)
-library(httr)
-library(RCurl)
-
-E5_weight <- read.csv(text = getURL("https://github.com/derele/Eimeria_Lab/blob/master/data/3_recordingTables/E6_062018_Eim_full_RECORDweight.csv"))
-E5_oocyst <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/3_recordingTables/E6_062018_Eim_full_RECORDoocysts.csv"))
