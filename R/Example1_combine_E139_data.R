73306534a19c974b80c5c3b0251a900b1b477fb3
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2020-12-04 12:20:00 +0100

    An example summarizing the evolution of pathogenicity of E139

--- R/Example1_combine_E139_data.R
+++ R/Example1_combine_E139_data.R
@@ -1,6 +1,9 @@
+### This could be an example of how to access particular subsets of
+### the data
 library(RCurl)
 library(dplyr)
 library(magrittr)
+library(ggplot2)
 
 OV <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/Eimeria_Lab_overview.csv"))
 
@@ -25,18 +28,34 @@ E139W <- E139W[!unlist(lapply(E139W, is.null))]
 E139Shed <- lapply(OV[OV$E139, "shedding"], loadFromGH)
 E139Shed <- E139Shed[!unlist(lapply(E139Shed, is.null))]
 
-Reduce(intersect, lapply(E139W, colnames))
-## "labels" is the only common column name!  FIX this!!
+W139colnames <- Reduce(intersect, lapply(E139W, colnames))
+S139colnames <- Reduce(intersect, lapply(E139Shed, colnames))
 
-Reduce(intersect, lapply(E139Shed, colnames))
-lapply(E139Shed, colnames)
+E139W <- lapply(E139W, "[", W139colnames)
+E139Shed <- lapply(E139Shed, "[", S139colnames)
 
+W139 <- Reduce(rbind, E139W)
+S139 <- Reduce(rbind, E139Shed)
 
-## Once the files all have the same (number of) columns (and names),
-## you can do something like:
-#Reduce(rbind, E139W)
+### calculating max weight loss for each mouse
+as_tibble(W139) %>%
+    group_by(EH_ID) %>%
+    slice_min(n=1, order_by=weight) %>%
+    left_join(OV[, c("Experiment", "Date")],
+              by= c("experiment"="Experiment")) %>%
+    group_by(experiment) %>%
+    mutate(meanMaxWLat = mean(dpi)) %>%
+    mutate(meanMaxWL = mean(1-(weight/weight_dpi0))*100) %>%
+    select(experiment, Date, meanMaxWLat, meanMaxWL) %>%
+    slice_head(n=1) %>%
+    mutate(Date=as.Date(paste0("01/", Date), format="%d/%m/%Y")) ->
+    maxWL
 
-# until separate standardised files are made, this should work
+pdf("example_fig/E139_evol_maxWL.pdf", width=6, height=5)
+ggplot(maxWL, aes(Date, meanMaxWLat, size= meanMaxWL)) +
+    geom_point(color="red") +
+    scale_y_continuous("maximal weightloss at dpi (average)") +
+    scale_x_date("date of the experiment") +
+    scale_size_continuous("mean maximal\nweightloss in %")
+dev.off()
 
-keep <- c("EH_ID", "weight", "faces_weight", "relative_weight", "weight_dpi0")
-weight <- lapply(E139W, function(x) subset(x, select = intersect(keep, colnames(x))))
---
+++ example_fig/README_example_figs.txt
@@ -0,0 +1,4 @@
+Figures from running examples will be stored here. Create them locally
+by running the examples, they are ignored (excluded) from the github
+repository.
+
