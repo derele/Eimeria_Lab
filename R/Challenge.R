ca5dfbd410d60037d60c5b4065d00a7e36a00dbc
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2021-09-23 22:10:23 +0200

    Cleaned oocyst data

--- R/Challenge.R
+++ R/Challenge.R
@@ -16,37 +16,37 @@ loadFromGH <- function(URL){
     }
 }
 
+## Only the challenge experiments
 ChallengeEx  <- c("E57", "E10", "E11")
 ExpNames <- OV$Experiment[OV$Experiment%in%ChallengeEx]
 
-## create a list of dataframes for the weight data subsetting for only
-## the experiments with the E139 used
 W <- lapply(OV[OV$Experiment%in%ChallengeEx, "weight"], loadFromGH)
 names(W) <- ExpNames
-
-
-W.cols <- Reduce(intersect, lapply(W, colnames))
-
-
-W[["E57"]] <- W[["E57"]][, W.cols]
-W[["E11"]] <- W[["E11"]][, W.cols]
-W[["E10"]] <- W[["E10"]][, W.cols]
-
-write.csv(W[["E57"]],
-          "data/Experiment_results/E57_xxxxx_Eim_record.csv",
-          row.names=FALSE)
-
-write.csv(W[["E11"]], "data/Experiment_results/E11_012021_Eim_record.csv",
-          row.names=FALSE)
-
-
-write.csv(W[["E10"]], "data/Experiment_results/E10_112020_Eim_record.csv",
-          row.names=FALSE)
 
 
 ## Same for shedding
 O <- lapply(OV[OV$Experiment%in%ChallengeEx, "shedding"], loadFromGH)
 names(O) <- ExpNames
+
+O[["E10"]] <- O[["E10"]][, !colnames(O[["E10"]])%in%"X"]
+
+O[["E11"]]$labels <- paste0("E11", O[["E11"]]$batch, O[["E11"]]$labels)
+O[["E11"]]$dilution <- 1
+
+OO.cols <- Reduce(intersect, lapply(O, colnames))
+
+O[["E10"]] <- O[["E10"]][, OO.cols]
+O[["E11"]] <- O[["E11"]][, OO.cols]
+O[["E57"]] <- O[["E57"]][, OO.cols]
+
+write.csv(O[["E57"]], "data/Experiment_results/E57_xxxxx_Eim_oocyst.csv",
+          row.names=FALSE)
+
+write.csv(O[["E10"]], "data/Experiment_results/E10_112020_Eim_oocyst.csv",
+          row.names=FALSE)
+
+write.csv(O[["E11"]], "data/Experiment_results/E11_Oocyst_CSV.csv",
+          row.names=FALSE)       
 
 ## Same for design
 D <- lapply(OV[OV$Experiment%in%ChallengeEx, "design"], loadFromGH)
