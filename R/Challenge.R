cfbfd1284d472fe106b865f689f0b0db91bd6c50
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2021-09-24 09:17:17 +0200

    Inclding E7 design into E57 design table

--- R/Challenge.R
+++ R/Challenge.R
@@ -24,41 +24,89 @@ ChallengeEx  <- c("E57", "E10", "E11")
 W <- lapply(OV[OV$Experiment%in%ChallengeEx, "weight"], loadFromGH)
 Weight <- Reduce(rbind, W)
 
+## messed up some lables for E7 challenge
+Weight[grepl("aE7", Weight$labels),]
+
+
 ## Same for shedding
 O <- lapply(OV[OV$Experiment%in%ChallengeEx, "shedding"], loadFromGH)
 Oocysts <- Reduce(rbind, O)
 
+## The same for oocysts
+head(Oocysts[grepl("aE7", Oocysts$labels), ])
+
+## some don't agree
+table(Oocysts$labels%in%Weight$labels)
+table(Weight$labels%in%Oocysts$labels)
+
+## But that's not the cause for the bigger problems as result are
+## still okay, as both the labels are messed up in the same way:
 
 Results <- merge(Weight, Oocysts, all=TRUE)
 ## IDs sometimes with "_" sometimes without
 Results$EH_ID <- gsub("LM_", "LM", Results$EH_ID)
 
-## We have some NAs in the mouse IDs here: 
+## BUT we now have some NAs in the mouse IDs here:
 table(is.na(Results$EH_ID))
 Results[is.na(Results$EH_ID), ]
-
-## Labels for which in E11 (both first and challenge infection) no
-## weight was in the table and thus tube labels have no mouse EH_ID
-## association. FIX ME!!!
+## These are labels for which in E11 (both first and challenge
+## infection) no weight was in the table and thus tube labels have no
+## mouse EH_ID association. FIX ME!!!
 
 ## For now we hav to exclude those
 Results <- Results[!is.na(Results$EH_ID), ]
 
+DesignE7 <- loadFromGH("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experimental_design/E7_112018_Eim_DESIGN.csv")
 
 ## Same for design
 D <- lapply(OV[OV$Experiment%in%ChallengeEx, "design"], loadFromGH)
 
 Des.cols <- Reduce(intersect, lapply(D, colnames))
+DesignE5 <- D[[1]]
+
+table(DesignE5$EH_ID%in%DesignE7$EH_ID)
+E7IDs <- DesignE7$EH_ID[DesignE7$EH_ID%in%DesignE5$EH_ID]
+
+DesignE5 <- DesignE5[!DesignE5$EH_ID%in%E7IDs, ]
+colnames(DesignE7)
+
+D57.cols <- intersect(colnames(DesignE5), colnames(DesignE7))
+
+DesignE57 <- rbind(DesignE5[, D57.cols], DesignE7[, D57.cols])
+
+write.csv(DesignE57, "data/Experimental_design/E57_xxxxx_Eim_DESIGN.csv",
+          row.names=FALSE)
+
+
 Design <- Reduce(rbind, lapply(D, "[", Des.cols))
+
 
 ## remove all whitespaces
 Design %>%
     mutate(across(where(is.character), str_trim)) ->
     Design
 
+Design[Design$experiment%in%c("E5", "E7"),]
+
 ## IDs sometimes with "_" sometimes without
 Design$EH_ID <- gsub("LM_", "LM", Design$EH_ID)
 
+#### NOW ALSO REMOVE EXPERIMENT AND ADD E7 Design to Design!
+
 ALL <- merge(Design, Results, all=TRUE)
 
+### some mice don't have an infection history 
+forgotten.mice <- unique(ALL[is.na(ALL$primary_infection), "EH_ID"])
+
+length(intersect(Design$EH_ID, Results$EH_ID))
+
+length(union(Design$EH_ID, Results$EH_ID))
+
+Design$EH_ID[!Design$EH_ID%in%Results$EH_ID]
+### Two samples with now result records
+### [1] "LM0205" "LM0290"
+
+
 write.csv(ALL, "data_products/Challenge_infections.csv")
+
+
