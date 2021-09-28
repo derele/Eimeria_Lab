4e2f743e9cd7a4c376ff12fc1c34401b3bd9dc49
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2021-09-28 11:12:24 +0200

    Franzi's update integrated in output table, new table highlighting problems exported...

--- R/Challenge.R
+++ R/Challenge.R
@@ -24,10 +24,7 @@ ChallengeEx  <- c("E57", "E10", "E11")
 W <- lapply(OV[OV$Experiment%in%ChallengeEx, "weight"], loadFromGH)
 Weight <- Reduce(rbind, W)
 
-
 ## ## Same for shedding
-
-
 O <- lapply(OV[OV$Experiment%in%ChallengeEx, "shedding"], loadFromGH)
 Oocysts <- Reduce(rbind, O)
 
@@ -37,21 +34,22 @@ table(Weight$labels%in%Oocysts$labels)
 
 ### FIXME!!!  It's experiment E11!!!
 Oocysts[!Oocysts$labels%in%Weight$labels, ]
+## FIXED by Franzi, just two labels missing: E11aBMI and E11aJOY
+
+## there are more in in the weights table which are not found in the
+## oocysts though
 Weight[!Weight$labels%in%Oocysts$labels, ]
+## Let's export this for Franzi to have a look:
+write.csv(Weight[!Weight$labels%in%Oocysts$labels, ],
+          "data/Experiment_results/FIXME_E11_Weight.csv", row.names=FALSE)
 
 Results <- merge(Weight, Oocysts, all=TRUE)
 
 ## IDs sometimes with "_" sometimes without
 Results$EH_ID <- gsub("LM_", "LM", Results$EH_ID)
 
-## BUT we now have some NAs in the mouse IDs here:
-table(is.na(Results$EH_ID))
-Results[is.na(Results$EH_ID), ]
-## These are labels for which in E11 (both first and challenge
-## infection) no weight was in the table and thus tube labels have no
-## mouse EH_ID association. Again: FIX ME!!!
-
-## For now we hav to exclude those
+## For now we hav to exclude those E11aBMI and E11aJOY which have no
+## mouse IDs
 Results <- Results[!is.na(Results$EH_ID), ]
 
 ## Same for design
@@ -66,39 +64,28 @@ Design %>%
     mutate(across(where(is.character), str_trim)) ->
     Design
 
-Design[Design$experiment%in%c("E5", "E7"),]
-
 ## IDs sometimes with "_" sometimes without
 Design$EH_ID <- gsub("LM_", "LM", Design$EH_ID)
 
-#### NOW ALSO REMOVE EXPERIMENT AND ADD E7 Design to Design!
-
+#### NOW ALSO REMOVE EXPERIMENT column?
+### Not for now
 ALL <- merge(Design, Results, all=TRUE, by="EH_ID")
+table(ALL$experiment.x, ALL$experiment.y)
+
+## let's call experiment 5 and 7 57, like in all tables
+
+Design$experiment[Design$experiment%in%c("E5", "E7")] <- "E57"
+Results$experiment[Results$experiment%in%c("E5", "E7")] <- "E57"
+ALL <- merge(Design, Results, all=TRUE)
 
 ### some mice don't have an infection history 
 forgotten.mice <- unique(ALL[is.na(ALL$primary_infection), "EH_ID"])
-
-length(intersect(Design$EH_ID, Results$EH_ID))
-
-length(union(Design$EH_ID, Results$EH_ID))
+## one mouse "LM0295" got lost?
 
 Design$EH_ID[!Design$EH_ID%in%Results$EH_ID]
-### Two samples with now result records
+### Two samples with no result records
 ### [1] "LM0205" "LM0290"
 
-table(ALL$experiment.x, ALL$experiment.y)
 
-
-## write.csv(ALL, "data_products/Challenge_infections.csv")
-
-
-## oldE11 <- read.csv("data/Experiment_results/E11_Oocyst_CSV.csv")
-
-## newE11 <- read.delim("data/Experiment_results/E11_Oocyst_updateCSV.csv", sep=";")
-
-## newE11$labels <- paste0("E11", newE11$batch, newE11$labels)
-## newE11$dilution <- 1
-
-write.csv(newE11[,colnames(oldE11)],
-          "data/Experiment_results/E11_Oocyst_updateCSV.csv", row.names=FALSE)
+write.csv(ALL, "data_products/Challenge_infections.csv", row.names=FALSE)
 
