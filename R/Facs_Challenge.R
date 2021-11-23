58849f0640a3ecd669395d64a6e076fb615652d5
fayweb <fy.webster@gmail.com>
2021-11-23 18:22:40 +0100

    Combined the challenge infection, FACS and infection intensity data

--- R/Facs_Challenge.R
+++ R/Facs_Challenge.R
@@ -7,15 +7,20 @@ library(purrr)
 #load the challenge infection table 
 Challenge <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data_products/Challenge_p4_p3.csv")
 
-#load the facs data for P4
+#load the infection intensity data
+P4_infection <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_CEWE_qPCR.csv") 
+E57_infection <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/E7_112018_Eim_CEWE_qPCR.csv") %>%
+  select(!X)
+P3_infection <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P3_112019_Eim_CEWE_qPCR.csv") %>%
+  select(!X)
+
+#load the facs data for P4, E57, E11
 P4_FACS <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_FACS.csv") %>%
   select(!X)
 
-#load the E57 Facs data
 E57_FACS <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/E7_112018_Eim_FACS.csv") %>%
   select(!X)
 
-#load the E11 FACS data
 E11_FACS <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/E11_012021_Eim_FACS.csv") %>%
   select(!X) %>%
   select(!X.1)
@@ -30,6 +35,13 @@ replace_LM <- function (x) {
 P4_FACS$EH_ID <- replace_LM(P4_FACS)
 E57_FACS$EH_ID <- replace_LM(E57_FACS)
 E11_FACS$EH_ID <- replace_LM(E11_FACS)
+P4_infection$EH_ID <- replace_LM(P4_infection)
+E57_infection$EH_ID <- replace_LM(E57_infection)
+P3_infection$EH_ID <- replace_LM(P3_infection)
+
+#Eim_MC in some infection intensity column is a character and in others is logical
+#change it so it is the same
+P4_infection$Eim_MC <- as.character(P4_infection$Eim_MC)
 
 #write a function to create a list of common column names
 names_common <- function(x, y) {
@@ -44,8 +56,11 @@ join_my_tables <- function (x, y) {
 
 #try to use the reduce to bind the tables together
 #reduce works like apply, but you need to have a list for it to work on
-Challenge_FACS <- list(Challenge, P4_FACS, E57_FACS, E11_FACS) %>%
+Challenge_intensity <- list(Challenge, P4_infection, E57_infection, P3_infection) %>%
   reduce(join_my_tables)
 
-#write the table
-write.csv(Challenge_FACS, "~/Documents/GitHub/Eimeria_Lab/data_products/Challenge_FACS", row.names = FALSE)
+Challenge_FACS_intensity <- list(Challenge_intensity, P4_FACS, E57_FACS, E11_FACS) %>%
+  reduce(join_my_tables)
+colnames(Challenge_FACS_intensity)
+
+write.csv(Challenge_FACS_intensity, "~/Documents/GitHub/Eimeria_Lab/data_products/FACS_intensity.csv", row.names = FALSE)
