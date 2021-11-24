5c58c7484ddfd10146d6c86713f6011d90a9a391
fayweb <fy.webster@gmail.com>
2021-11-24 19:34:41 +0100

    made some changes to reflect the changes caused by challenge.R. Will only use the file for my presentation tomorrow

--- R/Challenge_infection_intensities.R
+++ R/Challenge_infection_intensities.R
@@ -7,19 +7,6 @@ library(magrittr)
 library(janitor)
 library(purrr)
 
-#read challenge infections
-Challenge_Infections <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data_products/Challenge_infections.csv")
-
-#read experimental design P4
-P4_Design <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experimental_design/P4_082020_Eim_DESIGN.csv")
-P4_Design$EH_ID <- str_replace_all(P4_Design$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
-
-#read Experimental record P4
-P4_record <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_record.csv") %>%
-  select(!X)
-
-P4_record$EH_ID <- str_replace_all(P4_record$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
-
 #write a function to create a list of common column names
 names_common <- function(x, y) {
   intersect(colnames(x), colnames(y))
@@ -33,15 +20,35 @@ join_my_tables <- function (x, y) {
 
 #write a function to replace LM_ with LM
 replace_LM <- function (x) {
- str_replace_all(x$EH_ID, "LM_", "LM") 
+  str_replace_all(x$EH_ID, "LM_", "LM") 
 }
+
+# I can use this as a sample code, but please use the challenge.R 
+#as a base to add data 
+#read challenge infections
+Challenge_Infections <- read.csv(
+  "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data_products/Challenge_infections.csv")
+
+#read experimental design P4
+P4_Design <- read.csv(
+  "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experimental_design/P4_082020_Eim_DESIGN.csv")
+P4_Design$EH_ID <- str_replace_all(P4_Design$EH_ID, "LM_", "LM") 
+#replace string values in EH_ID Column so that table matches the challenge infections table
+
+#read Experimental record P4
+P4_record <- read.csv(
+  "https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_record.csv")
+
+P4_record$EH_ID <- str_replace_all(P4_record$EH_ID, "LM_", "LM") 
+#replace string values in EH_ID Column so that table matches the challenge infections table
+
+
 
 #join P4_Design and P4_record
 P4_Des_Rec <- join_my_tables(P4_record, P4_Design)
 
 #add the oocyst counts
-P4_oocysts <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_oocyst.csv") %>%
-  select(!X)
+P4_oocysts <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_oocyst.csv")
 
 #join the oocyst counts to the P4_Des_Rec
 P4_Experiment <- join_my_tables(P4_Des_Rec, P4_oocysts)
@@ -114,10 +121,11 @@ P3_Experiment %>%
 #bind the P4, P3 experiments to the challenge infections
 #error oocyst_mean in one table as character and in the other as factor
 P3_Experiment$oocyst_mean <- as.character(P3_Experiment$oocyst_mean)
+
 Challenge_infections_with_p3_p4 <- bind_rows(Challenge_infections_with_p4, P3_Experiment)
 
 
-#load the infection intensity data
+#load the infection intensity data (qPCR)
 P4_infection <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_CEWE_qPCR.csv") 
 E57_infection <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/E7_112018_Eim_CEWE_qPCR.csv") %>%
   select(!X)
@@ -139,3 +147,4 @@ Challenge_intensity <- list(Challenge_infections_with_p3_p4, P4_infection, E57_i
 
 #write the combination table
 write.csv(Challenge_intensity, "~/Documents/GitHub/Eimeria_Lab/data_products/Challenge_infection_intensity.csv", row.names = FALSE)
+
